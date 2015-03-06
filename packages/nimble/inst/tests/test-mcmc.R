86f4572a9de4c77bb770420e1adddb5a05b08383
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2015-03-05 16:58:30 -0800

    added note to ice examle in test-mcmc.R indicating poor mixing

--- packages/nimble/inst/tests/test-mcmc.R
+++ packages/nimble/inst/tests/test-mcmc.R
@@ -106,7 +106,7 @@ test_mcmc('ice', model = 'icear.bug', inits = 'ice-inits.R',
 # rework ice example so that beta[1] and beta[2] will be top nodes
 system(paste("sed 's/tau\\*1.0E-6/1.0E-6/g'", system.file('classic-bugs','vol2','ice','icear.bug', package = 'nimble'), ">", file.path(tempdir(), "icear.bug"))) 
 test_mcmc(model = file.path(tempdir(), "icear.bug"), inits = system.file('classic-bugs', 'vol2', 'ice','ice-inits.R', package = 'nimble'), data = system.file('classic-bugs', 'vol2', 'ice','ice-data.R', package = 'nimble'), numItsC = 1000, resampleData = TRUE)
-# looks fine, but alpha and beta values shifted a bit (systematically) relative to JAGS results
+# looks fine, but alpha and beta values shifted a bit (systematically) relative to JAGS results - on further inspection this is because mixing for this model is poor in both NIMBLE and JAGS - with longer runs they seem to agree (as best as one can tell given the mixing without doing a super long run)
 
 test_mcmc('beetles', model = 'beetles-logit.bug', inits = 'beetles-inits.R',
               data = 'beetles-data.R', numItsC = 1000, resampleData = TRUE)
