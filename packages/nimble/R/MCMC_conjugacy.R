535f74f3dacf0fca1e15448062a51093b83e151e
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2015-09-28 19:34:47 -0700

    moved GNUmakefile out of pkg directory and reworked location of Eigen_local so not at top; also revised configure{,.ac,.win} to work with inst/Eigen_local

--- packages/nimble/R/MCMC_conjugacy.R
+++ packages/nimble/R/MCMC_conjugacy.R
@@ -1014,8 +1014,8 @@ buildConjugateSamplerFunctions <- function(writeToFile = NULL) {
 }
 
 
-# buildConjugateSamplerFunctions(writeToFile = 'TEMP_conjugateSamplerDefinitions.R')
-buildConjugateSamplerFunctions()
+buildConjugateSamplerFunctions(writeToFile = 'TEMP_conjugateSamplerDefinitions.R')
+
 
 
 
