00f80bef2c2bd5f6e0e31eccee6f05dd054e3059
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2015-09-28 19:49:14 -0700

    removed ::: where not needed

--- packages/nimble/R/all_utils.R
+++ packages/nimble/R/all_utils.R
@@ -108,9 +108,9 @@ getsize <- function(container) {
 # simply adds width.cutoff = 500 as the default to deal with creation of long variable names from expressions
 deparse <- function(...) {
     if("width.cutoff" %in% names(list(...))) {
-        base:::deparse(...)
+        base::deparse(...)
     } else {
-          base:::deparse(..., width.cutoff = 500L)
+          base::deparse(..., width.cutoff = 500L)
       }
 }
 
