6d0132da2e0f7015b1e7b3a53d8c649e49ca0c3d
danielturek <dturek@berkeley.edu>
2016-03-16 10:35:53 -0700

    added checking for coeff is identity in dmnorm conj

--- packages/nimble/R/miscFunctions.R
+++ packages/nimble/R/miscFunctions.R
@@ -1,7 +1,7 @@
 # used in altParams for dmnorm
 # this needs to be sourced after nimbleFunction() is defined, so can't be done in distributions_inputList.R
 calc_dmnormAltParams <- nimbleFunction(
-    run = function(cholesky = double(2), prec_param = double(0), return_prec = double(0)) {
+    run = function(cholesky = double(2), prec_param = double(), return_prec = double()) {
         ## original (inefficient) implementation:
         ## tmp <- t(cholesky) %*% cholesky
         ## if(prec_param == return_prec) {
@@ -17,6 +17,30 @@ calc_dmnormAltParams <- nimbleFunction(
         }
         returnType(double(2))
         return(ans)
-    })
+    }
+)
+
+
+
+
+## used in conjugacy definition for dmnorm, to calculate 'contribution' terms;
+## avoids unnecessary matrix multiplications, when 'coeff' is identity matrix
+calc_dmnormConjugacyContributions <- nimbleFunction(
+    run = function(coeff = double(2), prec = double(2), order = double()) {
+        d <- dim(coeff)[1]
+        total <- 0
+        for(i in 1:d) {
+            for(j in 1:d) {
+                if(i == j) { total <- total + abs(coeff[i,j] - 1)
+                } else     { total <- total + abs(coeff[i,j])     }
+            }
+        }
+        if(total < d * 1E-15) return(prec)
+        if(order == 1) ans <- t(coeff) %*% prec
+        if(order == 2) ans <- t(coeff) %*% prec %*% coeff
+        return(ans)
+        returnType(double(2))
+    }
+)
 
 
