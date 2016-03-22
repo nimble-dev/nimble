72fe00b885dfda090ff6bccd331d307b201db8ae
danielturek <dturek@berkeley.edu>
2016-03-22 16:28:57 -0700

    added comment for better implementation

--- packages/nimble/R/miscFunctions.R
+++ packages/nimble/R/miscFunctions.R
@@ -2,18 +2,14 @@
 # this needs to be sourced after nimbleFunction() is defined, so can't be done in distributions_inputList.R
 calc_dmnormAltParams <- nimbleFunction(
     run = function(cholesky = double(2), prec_param = double(), return_prec = double()) {
-        ## original (inefficient) implementation:
-        ## tmp <- t(cholesky) %*% cholesky
-        ## if(prec_param == return_prec) {
-        ##     return(tmp)
-        ## } else {
-        ##     return(inverse(tmp))
-        ## }
         if(prec_param == return_prec) {
             ans <- t(cholesky) %*% cholesky
         } else {
             I <- identityMatrix(dim(cholesky)[1])
             ans <- backsolve(cholesky, forwardsolve(t(cholesky), I))
+            ## Chris suggests:
+            ## tmp <- forwardsolve(L, I)
+            ## ans <- crossprod(tmp)
         }
         returnType(double(2))
         return(ans)
