658aafc92bdc9064bf6215cbf18252c1f2e8df9a
perrydv <perry@Perrys-iMac.local>
2016-06-03 11:45:39 -0700

    adding BUGS_nimbleGraph.R content

--- packages/nimble/R/BUGS_nimbleGraph.R
+++ packages/nimble/R/BUGS_nimbleGraph.R
@@ -1,1 +1,36 @@
+nimbleGraphClass <- setRefClass(
+    "nimbleGraphClass",
+    fields = list(
+        graphExtPtr = 'ANY'
+    ),
+    methods = list(
+        setGraph = function(edgesFrom, edgesTo, edgesFrom2ParentExprIDs, types, names, numNodes) {
+            for(i in list(edgesFrom, edgesTo, edgesFrom2ParentExprIDs, numNodes)) {
+                if(length(i) > 0) {
+                    if(any(abs(i - round(i)) > 0.1)) {
+                        cat('caught something wrong on input to setGraph')
+                        browser()
+                    }
+                }
+            }
+            graphExtPtr <<- .Call('setGraph', edgesFrom, edgesTo, edgesFrom2ParentExprIDs, types, names, numNodes)
+        },
+        anyStochDependencies = function() {
+           .Call("anyStochDependencies",graphExtPtr)
+        },
+        anyStochParents = function() {
+            .Call("anyStochParents",graphExtPtr)
+        },
+        getDependencies = function(nodes, omit = integer(), downstream = FALSE) {
+            for(i in list(nodes, omit)) {
+                if(length(i) > 0) {
+                    if(any(abs(i - round(i)) > 0.1)) {
+                        cat('caught something wrong on input to setGraph')
+                        browser()
+                    }
+                }
+            }
 
+            .Call("getDependencies", graphExtPtr, nodes, omit, downstream)
+        }
+    ))
