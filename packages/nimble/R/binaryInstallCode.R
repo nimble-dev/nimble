0cee4cc81afe2b3e54f4895c0804d94c85b9cc52
Duncan Temple Lang <duncan@r-project.org>
2016-06-06 13:13:56 -0700

    much of this we don't need if we use static libraries.

--- packages/nimble/R/binaryInstallCode.R
+++ packages/nimble/R/binaryInstallCode.R
@@ -1,26 +1,50 @@
+#
+# The code in this file is to deal with a binary installation.
+# <aside>I'll say it again. This is a lot of work for no good reason. People installing
+#  nimble who want to use it are overwhelmingly going to use it to compile code generated
+#  by nimble and so they will need to have the developer tools installed. So they could just install
+#  the package from source and none of this would be a problem.
+#  Similarly, we can fix this with a simple change to R, i.e., a script that gets run when we install a binary
+#  package and then we can change settings (compile, link and run-time locations). (I should have committed this a long time ago - mea culpa).
+#</aside>
+
 
 .onLoad =
+    # We don't have to do this on load, but just at the first time the library is needed.
 function(...)
 {
    if(!sameDir(system.file(package = "nimble"), AutoconfInfo$rpkg_install_dir)) {
-
-       tryCatch(rebakeSOPath(libnimbleDLL()),
-                 error = function(e) {
+       message("Package was moved after installation")
+       NimbleSessionInfo$soDir = system.file("CppCode", package = "nimble")
+       NimbleSessionInfo$pkgMoved = TRUE
+       if(FALSE && UseLibraryMakevars && Sys.info()["sysname"] == "Darwin")
+               # See if we have the write permissions to change the id in place.
+          tryCatch(rebakeSOPath(libnimbleDLL()),
+                     error = function(e) {
+                             # If we don't have permission to rewrite the dylib, then
                             copyNimbleDylib()
                          })
-   }
-   
+   } else
+       NimbleSessionInfo$pkgMoved = FALSE
 }
 
 
 copyNimbleDylib =
-function(tmpdir = tempdir())
+    #
+    # create a copy of the libnimble.dylib file in another directory.
+    # The expectation is that is called for a binary installation on OSX
+    # and where we don't have permission to write to the libnimble.dylib file
+    # e.g., this is a centrally installed package shared by multiple users.
+    #
+    #
+function(dir = getOption("LibNimbleDir", tempdir()))
 {
     NimbleSessionInfo$soDir = tmpdir
-    f = sprintf("%s/%s", tmpdir, basename(libnimbleDLL()))
+    f = sprintf("%s/%s", dir, basename(libnimbleDLL()))
     cat("Copying .so\n")       
-    file.copy(libnimbleDLL(), tmpdir)
-    # See if we have the write permissions to change the id in place.
+    file.copy(libnimbleDLL(), dir)
+    Sys.chmod(f, "0755")
+    # Change the permissions on the file.
     cat("Setting new id in .dylib\n")
     rebakeSOPath(f)
     f
@@ -48,11 +72,19 @@ function(filename)
   #
   # Should check the length of the name
   #
-  cmd = sprintf("install_name_tool -id  %s %s", filename, filename)
 
-# print( getCurrentSOName(filename) )  
-  system(cmd, intern = TRUE)
 # print( getCurrentSOName(filename) )
+#  cmd = sprintf("install_name_tool -id  %s %s", filename, filename)
+#  system(cmd, intern = TRUE)  
+#  err = textConnection("e", open = "w", local = TRUE)
+#  out = textConnection("o", open = "w", local = TRUE)
+  val = system2("install_name_tool", c("-id", filename, filename), stdout = NULL, stderr = NULL)
+  if(val != 0) {
+      e = simpleError("can't change install_name", call = NULL)
+      stop(e)
+  }
+# print( getCurrentSOName(filename) )
+  filename      
 }
 
 
