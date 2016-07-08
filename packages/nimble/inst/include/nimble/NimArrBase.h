978748a93d969b0ca85743c24b71941b307aaa53
perrydv <pdevalpine@berkeley.edu>
2016-07-08 00:46:15 -0700

    made #pragma only used for Windows

--- packages/nimble/inst/include/nimble/NimArrBase.h
+++ packages/nimble/inst/include/nimble/NimArrBase.h
@@ -7,7 +7,17 @@
 #include <typeinfo>
 #include <iostream>
 
+#ifdef _WIN32
+#define _WIN3264
+#endif
+
+#ifdef _WIN64
+#define _WIN3264
+#endif
+
+#ifdef _WIN3264
 #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#endif
 
 using std::vector;
 
