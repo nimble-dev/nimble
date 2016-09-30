df3ece260dbc14eed9ab16f7b9330b52b36e6c5e
perrydv <perry@Perrys-iMac.local>
2016-09-30 12:47:56 -0700

    exported Rmatrix2mvOneVar and added small roxygen entry

--- packages/nimble/R/types_util.R
+++ packages/nimble/R/types_util.R
@@ -221,6 +221,21 @@ Cmatrix2mvOneVar <- function(mat, mv, varName, k){
             stop('varName not found in modelValues')
 }
 
+#' Set values of one variable of a modelValues object from an R matrix
+#'
+#' Normally a modelValues object is accessed one "row" at a time.  This function allows all rows for one variable to set from a matrix with one dimension more than the variable to be set.
+#'
+#' @param mat Input matrix
+#'
+#' @param mv  modelValues object to be modified.
+#'
+#' @param varName Character string giving the name of the variable on \code{mv} to be set
+#'
+#' @param k Number of rows to use
+#'
+#' @export
+#' @details
+#' This function may be deprecated in the future when a more natural system for interacting with modelValues objects is developed.
 Rmatrix2mvOneVar <- function(mat, mv, varName, k){
 	if( mv$symTab$symbols[[varName]][['type']] == 'double'){
 		storage.mode(mat) <- 'double'
