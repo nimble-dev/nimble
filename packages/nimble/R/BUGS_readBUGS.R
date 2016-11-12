522ac486873405e005b62e58fc4a0127ea3df29b
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2016-11-12 13:18:59 -0800

    fixed to check of data input as df; now allow logical and numeric

--- packages/nimble/R/BUGS_readBUGS.R
+++ packages/nimble/R/BUGS_readBUGS.R
@@ -9,9 +9,10 @@ BUGSmodel <- function(code, name, constants=list(), dimensions=list(), data=list
       stop("BUGSmodel: 'dimensions' must be a named list")
     if(length(data) && sum(names(data) == ""))
         stop("BUGSmodel: 'data' must be a named list")
-    if(any(!sapply(data, is.numeric)))
+    if(any(!sapply(data, function(x) {
+        is.numeric(x) || is.logical(x) })))
         stop("BUGSmodel: elements of 'data' must be numeric and cannot be data frames")
-     ## constantLengths <- unlist(lapply(constants, length))
+    ## constantLengths <- unlist(lapply(constants, length))
     ## if(any(constantLengths > 1)) {
     ##     iLong <- which(constantLengths > 1)
     ##     message(paste0('Constant(s) ', paste0(names(constants)[iLong], sep=" ", collapse = " "), ' are non-scalar and will be handled as inits.'))
