5993e3c94a4ba6aa0c83f761c50441043a6822a9
danielturek <danielturek@gmail.com>
2017-01-06 15:32:40 -0500

    added warning about applying RW_block sampler to Dirichlet nodes, in MCMC_configuration.R

--- packages/nimble/R/MCMC_configuration.R
+++ packages/nimble/R/MCMC_configuration.R
@@ -196,6 +196,7 @@ print: A logical argument, specifying whether to print the ordered list of defau
                     if(multivariateNodesAsScalars) {
                         for(scalarNode in nodeScalarComponents) {
                             addSampler(target = scalarNode, type = 'RW') };     next }
+                    if(nodeDist == 'ddirch')   warning(paste0('RW_block sampler was applied to Dirichlet node \'', node, '\', which will not result in proper sampling behaviour. Try reparameterizing this node in terms of constituent gamma distributions.'))
                     addSampler(target = node, type = 'RW_block');     next }
 
                 ## node is scalar, non-end node
