c4f0ad77195da19a2b61a9179f822cb95c19cd44
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2017-04-20 15:03:09 -0700

    Make error messages more informative

--- packages/nimble/inst/include/nimble/Utils.h
+++ packages/nimble/inst/include/nimble/Utils.h
@@ -36,7 +36,7 @@ class nimbleTimerClass_ {
 #define PRINTF Rprintf
 #define NIMERROR error
 #define RBREAK(msg) {PRINTF(msg); return(R_NilValue);}
-#define NIM_ASSERT(cond, msg) { if(NIM_UNLIKELY(!(cond))) { NIMERROR(msg); }}
+#define NIM_ASSERT(cond, ...) { if(NIM_UNLIKELY(!(cond))) { NIMERROR("Error: " __VA_ARGS__); }}
 
 // code copied from nmath.h - useful utilities 
 # define MATHLIB_ERROR(fmt,x)		error(fmt,x);
