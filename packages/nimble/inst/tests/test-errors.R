558236a5ea5aa023ab3f2648e74e31d6b1ddca9b
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2017-04-20 17:05:52 -0700

    Add unit test for error handling

---
+++ packages/nimble/inst/tests/test-errors.R
@@ -0,0 +1,42 @@
+source(system.file(file.path('tests', 'test_utils.R'), package = 'nimble'))
+
+context("Testing of error handling.")
+
+test_that("Testing of error handling in SEXP_2_NimArr", {
+  # First construct a nimbleFunction.
+  nimFun <- nimbleFunction(
+    run = function (x = logical(1),
+                    y = integer(1),
+                    z = double(1)) {
+      return(x + y + z)
+      returnType(double(1))
+    }
+  )
+  
+  # Make sure the nimble function works on well formed inputs.
+  x <- c(FALSE, TRUE)
+  y <- c(2, 4)
+  z <- c(8.0, 16.0)
+  expectedResult <- c(10.0, 21.0)
+  expect_equal(nimFun(x, y, z), expectedResult)
+  
+  # Next compile the nimbleFunction.
+  compiledFun = compileNimble(nimFun)
+  expect_equal(compiledFun(x, y, z), expectedResult)
+  
+  # Finally check that compiledFun behaves as expected on invalid inputs.
+  wrong_type = c("x", "y")
+  wrong_shape1 = TRUE
+  wrong_shape2 = matrix(c(1, 2, 3, 4), 2, 2)
+  
+  # The commented-out lines currently do not trigger errors.
+  expect_error(compiledFun(wrong_type, y, z))
+  expect_error(compiledFun(x, wrong_type, z))
+  expect_error(compiledFun(x, y, wrong_type))
+  # expect_error(compiledFun(wrong_shape1, y, z))
+  # expect_error(compiledFun(x, wrong_shape1, z))
+  # expect_error(compiledFun(x, y, wrong_shape1))
+  expect_error(compiledFun(wrong_shape2, y, z))
+  # expect_error(compiledFun(x, wrong_shape2, z))
+  # expect_error(compiledFun(x, y, wrong_shape2))
+})
