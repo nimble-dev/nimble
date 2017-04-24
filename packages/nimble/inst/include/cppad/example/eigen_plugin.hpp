64a38388594c5e0a0174d34a08e21fd4a88a4028
Nicholas Michaud <nicholas.michaud@gmail.com>
2017-04-24 13:59:53 -0700

    actually added cppad folder into include/

---
+++ packages/nimble/inst/include/cppad/example/eigen_plugin.hpp
@@ -0,0 +1,28 @@
+// $Id: eigen_plugin.hpp 3804 2016-03-20 15:08:46Z bradbell $
+# ifndef CPPAD_EXAMPLE_EIGEN_PLUGIN_HPP
+# define CPPAD_EXAMPLE_EIGEN_PLUGIN_HPP
+/* --------------------------------------------------------------------------
+CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-16 Bradley M. Bell
+
+CppAD is distributed under multiple licenses. This distribution is under
+the terms of the
+                    GNU General Public License Version 3.
+
+A copy of this license is included in the COPYING file of this distribution.
+Please visit http://www.coin-or.org/CppAD/ for information on other licenses.
+-------------------------------------------------------------------------- */
+/*$
+$begin eigen_plugin.hpp$$
+$spell
+	eigen_plugin.hpp
+	typedef
+$$
+
+$section Source Code for eigen_plugin.hpp$$
+$srccode%cpp% */
+// Declaration needed so an Eigen vector is a simple vector
+typedef Scalar value_type;
+/* %$$
+$end
+*/
+# endif
