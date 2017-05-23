ccdba348e8fd7a7e68eae890c136c40eb93bf6a5
perrydv <perry@Perrys-iMac.local>
2017-05-23 09:34:02 -0700

    add .onLoad()

--- packages/nimble/R/zzz.R
+++ packages/nimble/R/zzz.R
@@ -1,3 +1,7 @@
+.onLoad <- function(libname, pkgname) {
+    packageStartupMessage('For more information on NIMBLE and a User Manual, please visit http://R-nimble.org')
+}
+
 if(!exists("NeedMakevarsFile"))
  NeedMakevarsFile = FALSE
 if(!exists("UseLibraryMakevars"))
