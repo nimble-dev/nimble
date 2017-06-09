5bc33f96d0ca795e6e6fd48d92bffd650820c124
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2017-06-08 17:53:10 -0700

    Add documentation to NimTfRunner

--- packages/nimble/inst/include/nimble/tensorflow.h
+++ packages/nimble/inst/include/nimble/tensorflow.h
@@ -8,6 +8,27 @@
 
 // This class runs a single tensorflow graph. Instances should be created as
 // static local variables of a nimbleFunction.
+// Example usage:
+//  NimArr<double, 1> axpy(NimArr<double, 1> ARG1_a_,
+//                         NimArr<double, 1> ARG2_x_,
+//                         NimArr<double, 1> ARG3_y_) {
+//    static NimTfRunner* runner = 
+//      NimTfBuilder("0123456789abcdef")  // The string is a graphDefBase64.
+//        .withInput("ARG1_a_")
+//        .withInput("ARG2_x_")
+//        .withInput("ARG3_y_")
+//        .withOutput("RESULT_")
+//        .build();
+//
+//    NimArr<double, 1> result;
+//    result->initialize(0.0, false, ARG1_x_.dimSize(0));
+//    runner->setInput(ARG1_a_);
+//    runner->setInput(ARG2_x_);
+//    runner->setInput(ARG3_y_);
+//    runner->run();
+//    runner->getOutput(result);
+//    return result;
+//  }
 class NimTfRunner {
   TF_Status* status_;
   TF_Graph* graph_;
