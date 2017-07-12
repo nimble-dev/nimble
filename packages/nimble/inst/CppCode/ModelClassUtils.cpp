9e596350547e340d9e80ebe7c7b3c0c77bee18f0
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2017-07-12 14:58:28 -0700

    Define -DR_NO_REMAP and prefix R symbols with Rf_ (#325)

--- packages/nimble/inst/CppCode/ModelClassUtils.cpp
+++ packages/nimble/inst/CppCode/ModelClassUtils.cpp
@@ -15,7 +15,7 @@ SEXP getModelValuesPtrFromModel (SEXP rPtr){
 }
 
 NimArrType** cGetModelElementPtr(SEXP Sextptr, SEXP Sname) {
-  if(!isString(Sname)) {
+  if(!Rf_isString(Sname)) {
     PRINTF("Error: Sname is not character!\n");
     return(NULL);
   }
@@ -50,9 +50,9 @@ SEXP getMVBuildName (SEXP rPtr){
 	string mVBuildName = (*modelValuesPtr).getMVBuildName();
 	if (mVBuildName == "missing")
 		PRINTF("Warning: buildName missing in modelValues! \nConstructor must assign the string buildName to the name which call a SEXP that builds modelValues object\n");
-	SEXP rName = allocVector(STRSXP, 1);
+	SEXP rName = Rf_allocVector(STRSXP, 1);
 	PROTECT(rName);
-	SEXP mvbn = mkChar(mVBuildName.c_str() );
+	SEXP mvbn = Rf_mkChar(mVBuildName.c_str() );
 	PROTECT(mvbn);
 	SET_STRING_ELT(rName, 0, mvbn );
 	UNPROTECT(2);
