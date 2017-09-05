bc5a127487dc87b2007a7d8c160314233bb21763
lponisio <lponisio@gmail.com>
2017-09-05 16:19:25 -0700

    added return of summary statistics

--- packages/nimble/R/runNimbleMCMC.R
+++ packages/nimble/R/runNimbleMCMC.R
@@ -1,17 +1,18 @@
 
 #' Builds R model and compiles C model, configures and compiles MCMC,
-#' and can optionally run the MCMC or return all the model and MCMC
+#' and can optionally run the MCMC or return the model and MCMC
 #' components
 #'
 #'
 #' @details
 #'
-#' Creates and runs an BUGS model.
+#' Creates and optionally runs a BUGS model.
 #' By default, this will run the provided model, record all samples
-#' and generates summary statistics.  This default behavior can be
+#' and generate summary statistics.  This default behavior can be
 #' altered via a variety of arguments.  Following execution of the
 #' MCMC algorithms, returns a named list containing \code{samples} and
-#' \code{summary}. If runMCMC = FALSE, returns the model components.
+#' \code{summary}. If runMCMC = FALSE, returns the model components
+#'     required to run the model outside the function.
 #'
 #' @param code The quoted code expression representing the model, such
 #'     as the return value from a call to \code{nimbleCode}).  No
@@ -20,7 +21,7 @@
 #' @param constants A named list giving values of constants for the
 #'     model.  This is the same as the \code{constants} argument which
 #'     would be passed to \code{nimbleModel}.  Default value is
-#'     list().
+#'     \code{list()}.
 #'
 #' @param data A named list giving the data values for the model.
 #'     This is the same as the \code{data} argument which would be
@@ -38,11 +39,10 @@
 #'     statistics calculated, and density and trace plots generated.
 #'     Default value is all top-level stochastic nodes of the model.
 #'
-#' @param niter Number of MCMC iterations to run.  This applies to all
-#'     MCMC algorithms in the suite.  Default value is 1000.
+#' @param niter Number of MCMC iterations to run. Default value is 10000.
 #'
 #' @param burnin Number of initial, post-thinning, MCMC iterations to discard.
-#' Default value is 10.
+#' Default value is 100.
 #'
 #' #' @param nchains Number MCMC chains to run.
 #' Default value is 3.
@@ -63,13 +63,14 @@
 #' @param runMCMC Logical argument, specifying whether to run the MCMC
 #'     or return the setup model objects Default value is FALSE.
 #'
-#' @return Returns a named list containing elements:
-#' samples: A matrix containing samples from each MCMC algorithm.
-#' summary: A matrix containing summary statistics for each variable and algorithm.
-#' If runMCMC = FALSE, returns a named list containing elements:
-#' R.model: NIMBLE model created by a call to  \code{nimbleModel()}
-#' C.mcmc: compiled NIMBLE model and MCMC created by a call to  \code{compileNimble()}
-#' mcmc.spec: defaut MCMC configuration for a given model created by a call to  \code{configureMCMC()}
+#' @return Returns a named list containing elements: samples: A matrix
+#'     containing samples from each MCMC algorithm.  summary: A matrix
+#'     containing summary statistics for each variable and algorithm.
+#'     If runMCMC = FALSE, returns a named list containing elements:
+#'     R.model: NIMBLE model created by a call to \code{nimbleModel()}
+#'     C.mcmc: compiled NIMBLE model and MCMC created by a call to
+#'     \code{compileNimble()} mcmc.spec: defaut MCMC configuration for
+#'     a given model created by a call to \code{configureMCMC()}
 #'
 #' @examples
 #' \dontrun{
@@ -77,62 +78,107 @@
 #'     mu ~ dnorm(0, 1)
 #'     x ~ dnorm(mu, 1)
 #' })
-#' output <- runNimbleMCMC(code,
+#' model.samples <- runNimbleMCMC(code,
 #'                     data = list(x=3),
 #'                     inits = list(mu=0),
-#'                     niter = 10000,
 #'                     monitors = 'mu')
+#'
+#' model.components <- runNimbleMCMC(code,
+#'                     data = list(x=3),
+#'                     inits = list(mu=0),
+#'                     runMCMC = FALSE,
+#'                     monitors = 'mu')
+#'
+#'
 #' }
 #'
 #' @author L. Ponisio
 #' @export
 
 runNimbleMCMC <- function(code,
-                          constants,
-                          data,
-                          inits,
-                          monitors,
-                          niter = 1000,
+                          constants=list(),
+                          data=list(),
+                          inits=list(),
+                          monitors=NULL,
+                          niter = 10000,
                           thin=1,
-                          burin=10,
+                          burnin=100,
                           nchains=3,
                           check = FALSE,
                           debug = FALSE,
                           runMCMC = TRUE, ...){
-    ## build model
+
+    ## Build the R model
     R.model <- nimbleModel(code = code,
                            constants = constants,
                            data = data,
                            inits = inits,
                            check = check,
                            debug = debug,...)
-    message('R model created')
-
-    ## configure and build mcmc
+    message('**** R model created ****')
+    if(is.null(monitors)){
+        monitors <- R.model$getNodeNames(topOnly=TRUE)
+    }
+    ## Configure and build mcmc
     mcmc.spec <- configureMCMC(R.model,
                                print = FALSE,
                                monitors = monitors,
                                thin = thin)
     mcmc <- buildMCMC(mcmc.spec)
-    message('MCMC built')
+    message('**** MCMC built ****')
 
-    ## compile model in C++
+    ## Compile model in C++
     C.model <- compileNimble(R.model)
     C.mcmc <- compileNimble(mcmc, project = R.model)
-    message('NIMBLE model compiled')
+    message('**** NIMBLE model compiled ****')
 
     if(runMCMC){
-        ## run model
-        message('running model')
-        samplesList <- runMCMC(C.mcmc, niter = niter, nburnin =
-                                                          burnin, nchains = nchains)
-        return(samplesList)
-    } else{
+        ## Runs the MCMC, returns the samples and summary statistics.
+        ## Run the model
+        message('**** Running model ****')
+        sample.list <- runMCMC(C.mcmc,
+                               niter = niter,
+                               nburnin = burnin,
+                               nchains = nchains)
+
+        calcSummaryStats <- function(samples){
+            ## Calculates mean, median, sd, and CI
+            means <- mean(samples)
+            sds <- sd(samples)
+            return(as.matrix(c(means,
+                               median(samples), sds,
+                               means - sds*1.96, means + sds*1.96)))
+        }
+        ## If the number of chains is > 1 takes calculates the summary
+        ## statistics across all of the chains.
+        ##
+        ## Extract the MCMC for each parameter from the different
+        ## chains, the caclulate the summary statistics.
+        getParSamples <- function(samples, par.name){
+            samples[, par.name]
+        }
+        summary.stats.all.chains <- sapply(monitors, function(par.name){
+            all.par.samples <- do.call(c,
+                                       lapply(sample.list, getParSamples,
+                                              par.name))
+            summary.stats <- calcSummaryStats(all.par.samples)
+            colnames(summary.stats) <- par.name
+            return(summary.stats)
+        })
+        ## Ronames are lost with multi parameter models if not set here
+        rownames(summary.stats.all.chains) <-
+            c('mean', 'median', 'sd', 'CI95_low', 'CI95_upp')
+
+        return(list(samples=sample.list,
+                    summary=summary.stats.all.chains))
+
+    }else{
+        ## Return the model components which can be used to run the
+        ## model using runMCMC outside this function.
         model.components <- list(R.model = R.model,
                                  mcmc.spec = mcmc.spec,
                                  C.mcmc = C.mcmc)
         return(model.components)
-        }
+    }
 }
-
 
