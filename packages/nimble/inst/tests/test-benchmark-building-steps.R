9f23874966d692d448a69f8f88f046f04abb0de7
perrydv <pdevalpine@berkeley.edu>
2017-09-22 11:25:14 -0700

    turn down size of benchmark-building-steps

--- packages/nimble/inst/tests/test-benchmark-building-steps.R
+++ packages/nimble/inst/tests/test-benchmark-building-steps.R
@@ -47,7 +47,7 @@ test_that('Benchmarking model and MCMC building and compiling steps',
 
     ## 1000 is a good size for full benchmarking
     ## 10 is a good size for routine testing
-    Benchmark1length <- 1000
+    Benchmark1length <- 10
     ## following will be like 'theta->mu[1:10]->y[1:10]'
     caseNames[1] <- paste0('theta->mu[1:',
                            Benchmark1length,
@@ -69,7 +69,7 @@ test_that('Benchmarking model and MCMC building and compiling steps',
 
     ## 100x20 is a good size for full benchmarking
     ## 5 x 2 is a good size for routing testing 
-    Benchmark2dims <- c(100, 20)
+    Benchmark2dims <- c(5, 2)
     ## following will be like 'theta->mu[1:5]->y[1:5, 1:2]'
     caseNames[2] <- paste0('theta->mu[1:',
                            Benchmark2dims[1],
