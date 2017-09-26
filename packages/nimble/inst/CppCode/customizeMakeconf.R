5d89e2970a0d4d262c050d6afc8093e5c13c3eed
perrydv <pdevalpine@berkeley.edu>
2017-09-26 16:55:07 -0700

    replace '-g ' not just '-g' (which could being a longer option)

--- packages/nimble/inst/CppCode/customizeMakeconf.R
+++ packages/nimble/inst/CppCode/customizeMakeconf.R
@@ -2,6 +2,6 @@ packageMakeconfFile <- file.path(R.home('etc'), 'Makeconf')
 if(file.exists(packageMakeconfFile)) {
     file.copy(packageMakeconfFile, 'Makeconf')
     lines <- readLines('Makeconf')
-    lines <- gsub('-g', '', lines)
+    lines <- gsub('-g ', ' ', lines)
     writeLines(lines, con = 'Makeconf')
 }
