0392332ae73ce70232b83a4581c9cca7951dc12d
Nicholas Michaud <nicholas.michaud@gmail.com>
2017-10-10 10:44:13 -0700

    Linker error resolved.

--- packages/nimble/inst/CppCode/nimbleCppAD.cpp
+++ packages/nimble/inst/CppCode/nimbleCppAD.cpp
@@ -3,7 +3,7 @@
 
 nimSmartPtr<NIMBLE_ADCLASS>  NIM_DERIVS_CALCULATE(NodeVectorClassNew &nodes, NimArr<1, double> &derivOrders) {
   nimSmartPtr<NIMBLE_ADCLASS> ADlist = new NIMBLE_ADCLASS;
-  return(ADlist)
+  return(ADlist);
   /* const vector<NodeInstruction> &instructions = nodes.getInstructions();
   vector<NodeInstruction>::const_iterator iNode(instructions.begin());
   vector<NodeInstruction>::const_iterator iNodeEnd(instructions.end());
@@ -15,7 +15,7 @@ nimSmartPtr<NIMBLE_ADCLASS>  NIM_DERIVS_CALCULATE(NodeVectorClassNew &nodes, Nim
 
 nimSmartPtr<NIMBLE_ADCLASS>  NIM_DERIVS_CALCULATE(NodeVectorClassNew &nodes,  int iNodeFunction, NimArr<1, double> &derivOrders) {
   nimSmartPtr<NIMBLE_ADCLASS> ADlist = new NIMBLE_ADCLASS;
-  return(ADlist)
+  return(ADlist);
   /* const vector<NodeInstruction> &instructions = nodes.getInstructions();
   vector<NodeInstruction>::const_iterator iNode(instructions.begin());
   vector<NodeInstruction>::const_iterator iNodeEnd(instructions.end());
