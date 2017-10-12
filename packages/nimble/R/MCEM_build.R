16924efa4952bb43ab0a0094be7acaa6f55308bb
Nicholas Michaud <nmichaud@berkeley.edu>
2017-10-12 09:48:42 -0700

    Add warning message for discrete top-level params in MCEM. (#620)
    * Add error message for discrete top-level params in MCEM.
    
    * Update NEWS.
    
    * Change warning to error.

--- packages/nimble/R/MCEM_build.R
+++ packages/nimble/R/MCEM_build.R
@@ -201,7 +201,9 @@ buildMCEM <- function(model, latentNodes, burnIn = 500 , mcmcControl = list(adap
   if(length(setdiff(latentNodes, allStochNonDataNodes) ) != 0 )
     stop('latentNodes provided not found in model')
   maxNodes = model$expandNodeNames(setdiff(allStochNonDataNodes, latentNodes), returnScalarComponents = TRUE)
-
+  if(any(model$isDiscrete(maxNodes))) stop(paste0("MCEM cannot optimize over discrete top-level parameters. The following top-level parameters in your model are discrete: ",
+                                                  paste0(maxNodes[model$isDiscrete(maxNodes)], collapse = ', ')))
+  
   limits <- getMCEMRanges(model, maxNodes, buffer)
   low_limits = limits[[1]]
   hi_limits  = limits[[2]]
@@ -272,7 +274,6 @@ buildMCEM <- function(model, latentNodes, burnIn = 500 , mcmcControl = list(adap
   cGetCov = compileNimble(RgetCov, project = Rmodel)  
   cvarCalc <- compileNimble(RvarCalc, project = Rmodel)
   cCalc_E_llk = compileNimble(Rcalc_E_llk, project = Rmodel)  
-  
   nParams = length(maxNodes)
   run <- function(initM = 1000){
     if(burnIn >= initM)
