e5a5c1cf736d44923f2502e9143692cef1993720
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2017-10-27 09:35:40 -0700

    fixed typo in test-mcmc

--- packages/nimble/inst/tests/test-tfize.R
+++ packages/nimble/inst/tests/test-tfize.R
@@ -4,6 +4,7 @@ source(system.file(file.path('tests', 'mathTestLists.R'), package = 'nimble'))
 context("Testing of Tensorflow-ization")
 
 ## note: need version of tensorflow recent enough to support tf$cosh
+## tensorflow 1.2.1 is too old
 ## on smeagol, make sure to do 'source activate r-tensorflow'
 
 test_that('Tensorflow implementation of axpy works', {
