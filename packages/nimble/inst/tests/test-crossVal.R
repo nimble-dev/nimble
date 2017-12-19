9fff6fcd181da73f745bc6d3951e914d6c4a4117
Nicholas Michaud <nicholas.michaud@gmail.com>
2017-12-18 16:33:29 -0800

    Add test for nBootReps = NA for crossVal algo.

--- packages/nimble/inst/tests/test-crossVal.R
+++ packages/nimble/inst/tests/test-crossVal.R
@@ -76,6 +76,37 @@ test_that("voter model cross-validation runs using MSE loss: ", {
                               nBootReps = 2)
 })
 
+test_that("cross-validation runs correctly when nBootReps = NA: ", {
+  y <- c(44.6, 57.76, 49.91, 61.34, 49.60, 61.79, 48.95, 44.70, 59.17, 53.94,
+         46.55, 54.74, 50.27, 51.24, 46.32)
+  growth <- c(2.4, 2.89, .85, 4.21, 3.02, 3.62, 1.08, -.39, 3.86, 2.27, .38,
+              1.04, 2.36, 1.72, .1)
+  N = length(y)
+  voterCode = nimbleCode({
+    for(i in 1:N){
+      y[i] ~ dnorm(beta_1 + growth[i]*beta_2, sd = sigma)
+    }
+    sigma ~ dunif(0,50)
+    beta_1 ~ dnorm(0, .01)
+    beta_2 ~ dnorm(0, .01)
+  })
+  dataList = list(y = y);
+  constList = list(growth = growth, N = N)
+  voterModel = nimbleModel(code = voterCode, data = dataList, 
+                           constants = constList,
+                           inits = list(beta_1 = 44, beta_2 = 3.75, 
+                                        sigma = 4.4))
+  voterConf <- configureMCMC(voterModel)
+  testOut <- runCrossValidate(MCMCconfiguration = voterConf,
+                              k = 2, 
+                              foldFunction = 'random',
+                              lossFunction = 'MSE',
+                              MCMCcontrol = list(niter = 100),
+                              nBootReps = NA)
+  expect_true(is.na(testOut$CVstandardError))
+})
+
+
 test_that("Radon model cross-validation runs using MSE loss: ", {
   url <- "http://stat.columbia.edu/~gelman/arm/examples/arsenic/wells.dat"
   wells <- try(read.table(url), silent = TRUE)
