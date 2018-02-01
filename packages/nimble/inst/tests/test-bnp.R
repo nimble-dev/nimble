11a161ea2440d85b0e11c30901e7af5af4c0912c
claudiawehrhahn <clau.wehrhahn@gmail.com>
2018-01-31 23:22:38 -0800

    model with gamma distributed conc node included

--- packages/nimble/inst/tests/test-bnp.R
+++ packages/nimble/inst/tests/test-bnp.R
@@ -151,6 +151,76 @@ for(i in 1:nrow(aux)){
 
 
 
+#--------------------------------------------------
+# model with random concentration parameter
+#-- BUGS definition of the model
+Code=nimbleCode(
+  {
+    for(i in 1:N3){
+      thetatilde[i] ~ dnorm(mean=mu0, var=tau20) 
+      s2tilde[i] ~ dinvgamma(shape=a0, scale=b0) 
+    }
+    xi[1:N2] ~ dCRP(conc)
+    conc ~ dgamma(1, 1)
+    
+    for(i in 1:N){
+      theta[i] <- thetatilde[xi[i]]
+      s2[i] <- s2tilde[xi[i]]
+      y[i] ~ dnorm(theta[i], var=s2[i])
+    }
+    a0<-1; b0<-0.5; mu0<-0; tau20<-40
+  }
+)
+
+conc<-1; a0<-1; b0<-0.5; mu0<-0; tau20<-40
+Consts=list(N=50, N2=50, N3=50)
+set.seed(1)
+aux=sample(1:10, size=Consts$N2, replace=TRUE)
+Inits=list(xi=aux, thetatilde=rnorm(Consts$N3, mu0, sqrt(tau20)),
+           s2tilde=rinvgamma(Consts$N3, shape=a0, scale=b0),
+           conc=1)
+
+s20=4; s21=4
+mu01=5; mu11=-5
+Data=list(y=c(rnorm(Consts$N/2,mu01,sqrt(s20)), rnorm(Consts$N/2,mu11,sqrt(s21))))
+
+#-- compiling the model:
+model<-nimbleModel(Code, data=Data, inits=Inits, constants=Consts,  calculate=TRUE)
+cmodel<-compileNimble(model)
+
+#-- standarized output:
+modelConf<-configureMCMC(model, print=FALSE, thin=10) # less samples!
+
+modelConf$addMonitors('xi')
+
+modelConf$removeSamplers(c("conc"), print="TRUE")
+modelConf$addSampler(c("conc"), type="sampler_Augmented_BetaGamma")
+modelConf$printSamplers(c("conc"))
+
+modelMCMC=buildMCMC(modelConf)
+#-- compiling the sampler
+CmodelNewMCMC=compileNimble(modelMCMC, project=model,
+                            resetFunctions=TRUE, showCompilerOutput = TRUE)
+#-- MCMC samples
+set.seed(1)
+nsave=100
+t1=proc.time()
+CmodelNewMCMC$run(nsave)
+proc.time()-t1
+
+
+mvSaved=CmodelNewMCMC$mvSamples
+
+SamplerG <- nimble:::sampler_G2(model, mvSaved)
+SamplerG$run()
+aux=as.matrix(SamplerG$mv)  ## the mv object is accessed here
+
+trunc=length(aux[1,])/3
+for(i in 1:nrow(aux)){
+  plot(aux[i, (trunc+1):(2*trunc)], aux[i, 1:trunc], type="h", main=sum(aux[i, 1:trunc])); readline()
+}
+
+
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
 #-- stick_breaking representation and BUGS Code
