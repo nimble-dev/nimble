19a86cce4f46e2d033d31a037579bccddb86b0d4
claudiawehrhahn <clau.wehrhahn@gmail.com>
2018-02-02 14:00:08 -0800

    help and NaN added to CRP distribution

--- packages/nimble/R/BNP_distributions.R
+++ packages/nimble/R/BNP_distributions.R
@@ -21,26 +21,95 @@
 #   output:
 #      x: a value
 
+#' The Chinesse Restaurant Process Distribution
+#'
+#'   Density and random generation for the Chinesse
+#'   Restaurant Process distribution with concentration 
+#'   parameter \code{conc}.
+#' 
+#' @name ChinesseRestaurantProcess 
+#' 
+#' @param x vector of values.
+#' @param n number of observations.
+#' @param conc scalar with concentration parameter.
+#' @param log logical; if TRUE, probability density is returned on the log scale.
+#' @author Claudia Wehrhahn
+#' @details The chineese restaurant process distribution is a distribution
+#' on the space of partitions of the positive integers. 
+#' The chineese restaurant process
+#' distribution with concentration parameter \eqn{=\alpha}{= conc} has 
+#' probability function 
+#' \deqn{f(x_i)=\frac{\alpha}{i-1+\alpha}}
+#' 
+#' If \code{conc} is not sepcified, it assumes the default value of 1. 
+#' @return \code{dCRP} gives the density, and \code{rCRP} gives random generation.
+#' @references Blackwell, D., & MacQueen, J. B. (1973) Ferguson distributions via 
+#' Pólya urn schemes \emph{The annals of statistics}, 353-355.
+#' 
+#' Aldous, D. J. (1985) Exchangeability and related topics \emph{In École d'Été de 
+#' Probabilités de Saint-Flour XIII} 1983 (pp. 1-198) Springer, Berlin, Heidelberg.
+#' 
+#' Pitman, Jim. Some developments of the Blackwell-MacQueen urn scheme 
+#' \emph{Statistics, probability and game theory}, 245--267, Institute of
+#'  Mathematical Statistics, Hayward, CA, 1996.
+#' 
+#' @examples
+#' x <- rCRP(10, conc = 1)
+#' dCRP(x, conc = 1)
+NULL
+
+#' @rdname ChinesseRestaurantProcess
+#' @export
 rCRP=nimbleFunction(
   run=function(n=integer(0), 
                conc=double(0, default=1))
   {
     returnType(double(1))
-    x=numeric(n)# returned vector:
-    x[1]=1
-    if(n>1){
-      for(i in 2:n){
-        if(runif(1)<=conc/(conc+i-1)){# a new value
-          x[i]=max(x[1:(i-1)])+1 
-        }else{# an old value
-          index=rcat(n=1, rep(1, i-1)) 
-          x[i]=x[index]
+    
+    n <- floor(n)
+    if( length(n) > 1){
+      n <- floor(n[1])
+      print('length(n) > 1 only the first element will be used')
+    }
+    
+    if( conc[1] <= 0 ){
+      concCond1 <- 0
+      print('conc parameter has to be larger than zero')
+    }else{
+      concCond1 <- 1
+    }
+    
+    if( length(conc) > 1 ){
+      concCond2 <- 0
+      print('conc parameter has to be a scalar')
+    }else{
+      concCond2 <- 1
+    }
+    
+    if(concCond1 == 0 ){
+      return(NaN)
+    }
+    if( concCond2 == 0 ){
+      return(NaN)
+    }
+    if( concCond1+concCond2 == 2){
+      x=numeric(n)# returned vector:
+      x[1]=1
+      if(n>1){
+        for(i in 2:n){
+          if(runif(1)<=conc/(conc+i-1)){# a new value
+            x[i]=max(x[1:(i-1)])+1 
+          }else{# an old value
+            index=rcat(n=1, rep(1, i-1)) 
+            x[i]=x[index]
+          }
         }
       }
+      return(x)
     }
-    return(x)
   }
 )
+
 
 
 dCRP=nimbleFunction(
@@ -52,26 +121,49 @@ dCRP=nimbleFunction(
     n=length(x) 
     tmpden=numeric(n) 
     
-    tmpden[1]=1
-    if(n>1){
-      for(i in 2:n){
-        #counts=0 # replaces sum(x[i]==x[1:(i-1)]) (works in nimble?)
-        #for(j in 1:(i-1)){
-        #  if(x[i]==x[j]){
-        #    counts=counts+1
-        #  }
-        #}
-        counts=sum(x[i]==x[1:(i-1)])
-        if(counts>0){
-          tmpden[i]=1/(i-1+conc)
-        }else{
-          tmpden[i]=conc/(i-1+conc)
+    if( conc[1] <= 0 ){
+      concCond1 <- 0
+      print('conc parameter has to be larger than zero')
+    }else{
+      concCond1 <- 1
+    }
+    
+    if( length(conc) > 1 ){
+      concCond2 <- 0
+      print('conc parameter has to be a scalar')
+    }else{
+      concCond2 <- 1
+    }
+    
+    if(concCond1 == 0 ){
+      return(NaN)
+    }
+    if( concCond2 == 0 ){
+      return(NaN)
+    }
+    if( concCond1+concCond2 == 2){
+      tmpden[1]=1
+      if(n>1){
+        for(i in 2:n){
+          #counts=0 # replaces sum(x[i]==x[1:(i-1)]) (works in nimble?)
+          #for(j in 1:(i-1)){
+          #  if(x[i]==x[j]){
+          #    counts=counts+1
+          #  }
+          #}
+          counts=sum(x[i]==x[1:(i-1)])
+          if(counts>0){
+            tmpden[i]=1/(i-1+conc)
+          }else{
+            tmpden[i]=conc/(i-1+conc)
+          }
         }
       }
+      logProb <- sum(log(tmpden))
+      if(log) return(logProb)
+      else return(exp(logProb)) 
     }
-    logProb <- sum(log(tmpden))
-    if(log) return(logProb)
-    else return(exp(logProb)) 
   }
 )
+
 
