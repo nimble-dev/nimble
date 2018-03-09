07e1642a3d2fbe0a9b27c1ad0feae677b1061073
claudiawehrhahn <clau.wehrhahn@gmail.com>
2018-03-09 15:03:57 -0800

    conjugacy and BNP model testing * in test-bnp-all.R file

--- packages/nimble/inst/tests/test-bnp-all.R
+++ packages/nimble/inst/tests/test-bnp-all.R
@@ -97,11 +97,205 @@ try(test_that("Test that random samples (rCRP) have correct mean of K: ",
 #                           info = "Difference in expected mean of K exceeds tolerance")))
 
 
-# check parameters: conc>0; in sampling check returned values
-
-
-
 
 ## Test stick_breaking nimble function:
 
+
+######################################################################
+######################################################################
+
+
+context("Testing of bnp conjugacy")
+
+#RwarnLevel <- options('warn')$warn
+#options(warn = -1)
+
+#source(system.file(file.path('tests', 'dynamicIndexingTestLists.R'), package = 'nimble'))
+
+#nimbleOptions(allowDynamicIndexing = TRUE)
+
+## check variations on use of dynamic indexing in BUGS code including building and compiling,
+## dependencies, and valid and invalid dynamic index values
+
+#ans1 <- sapply(testsDynIndex, test_dynamic_indexing_model)
+#ans2 <- sapply(testsInvalidDynIndex, test_dynamic_indexing_model)
+#ans3 <- sapply(testsInvalidDynIndexValue, test_dynamic_indexing_model)
+
+## check conjugacy detection
+
+test_that("Testing conjugacy detection with bnp models", { 
+  
+  code = nimbleCode({
+    for(i in 1:4) 
+      y[i] ~ dnorm(thetatilde[xi[i]], sd = 1)
+    xi[1:4] ~ dCRP(1)
+    for(j in 1:5) 
+      thetatilde[j] ~ dnorm(0, 1)
+  })
+  m = nimbleModel(code, data = list(y = rnorm(4)),
+                  inits = list(xi = rep(1,4), thetatilde=rnorm(5)))
+  conf <- configureMCMC(m)
+  expect_match(conf$getSamplers()[[1]]$name, "dCRP_conjugate_dnorm_dnorm",
+               info = "failed to detect normal-normal conjugacy")
+  
+  code = nimbleCode({
+    for(i in 1:4) 
+      y[i] ~ dnorm(thetatilde[xi[i]+1], sd = 1)
+    xi[1:(4+1)] ~ dCRP(1)
+    for(j in 1:5) 
+      thetatilde[j] ~ dnorm(0, 1)
+  })
+  m = nimbleModel(code, data = list(y = rnorm(4)),
+                  inits = list(xi = rep(1,4), thetatilde=rnorm(5)))
+  conf <- configureMCMC(m)
+  expect_match(conf$getSamplers()[[1]]$name, "dCRP_nonconjugate",
+               info = "failed to detect non conjugacy")
+  
+  code = nimbleCode({
+    for(i in 1:4) 
+      y[i] ~ dnorm(thetatilde[2*xi[i]], sd = 1)
+    xi[1:4] ~ dCRP(1)
+    for(j in 1:5) 
+      thetatilde[j] ~ dnorm(0, 1)
+  })
+  m = nimbleModel(code, data = list(y = rnorm(4)),
+                  inits = list(xi = rep(1,4), thetatilde=rnorm(5)))
+  conf <- configureMCMC(m)
+  expect_match(conf$getSamplers()[[1]]$name, "dCRP_nonconjugate",
+               info = "failed to detect non conjugacy")
+  
+  
+  code = nimbleCode({
+    for(i in 1:4) 
+      y[i] ~ dnorm(thetatilde[xi[i]], sd = 1)
+    xi[1:4] ~ dCRP(1)
+    for(j in 1:5) 
+      thetatilde[j] ~ dpois(1)
+  })
+  m = nimbleModel(code, data = list(y = rnorm(4)),
+                  inits = list(xi = rep(1,4), thetatilde=rpois(5, 1)))
+  conf <- configureMCMC(m)
+  expect_equal(length(grep("dCRP_conjugate", conf$getSamplers()[[1]]$name)), 0,
+               info = "incorrectly  conjugacy detected in poisson-normal case")
+  
+  # y1 and y2 are independent
+  code = nimbleCode({
+    for(i in 1:4){ 
+      y1[i] ~ dnorm(thetatilde1[xi1[i]], sd = 1)
+      y2[i] ~ dnorm(thetatilde2[xi2[i]], sd = 1)}
+    xi1[1:4] ~ dCRP(1)
+    xi2[1:4] ~ dCRP(1)
+    for(j in 1:5){
+      thetatilde1[j] ~ dnorm(0,1)
+      thetatilde2[j] ~ dnorm(0,1)}
+  })
+  m = nimbleModel(code, data = list(y1 = rnorm(4), y2 = rnorm(4)),
+                  inits = list(xi1 = rep(1,4), xi2 = rep(1,4), thetatilde1=rnorm(5), thetatilde2=rnorm(5)))
+  conf <- configureMCMC(m)
+  expect_match(conf$getSamplers()[[1]]$name, "dCRP_conjugate_dnorm_dnorm",
+               info = "failed to detect normal-normal conjugacy")
+  
+  code = nimbleCode({
+    for(i in 1:4){ 
+      y1[i] ~ dnorm(thetatilde[xi1[i]], sd = 1)
+      y2[i] ~ dnorm(thetatilde[xi2[i]], sd = 1)}
+    xi1[1:4] ~ dCRP(1)
+    xi2[1:4] ~ dCRP(1)
+    for(j in 1:5){
+      thetatilde[j] ~ dnorm(0,1)}
+  })
+  m = nimbleModel(code, data = list(y1 = rnorm(4), y2 = rnorm(4)),
+                  inits = list(xi1 = rep(1,4), xi2 = rep(1,4), thetatilde=rnorm(5)))
+  conf <- configureMCMC(m)
+  expect_match(conf$getSamplers()[[1]]$name, "dCRP_nonconjugate",
+               info = "failed to detect non conjugacy") # more than 1 dCRP distr
+  
+  code = nimbleCode({
+    for(i in 1:4){ 
+      y1[i] ~ dnorm(thetatilde[xi[i]], sd = 1)
+      y2[i] ~ dnorm(thetatilde[xi[i]], sd = 1)}
+    xi[1:4] ~ dCRP(1)
+    for(j in 1:5){
+      thetatilde[j] ~ dnorm(0,1)}
+  })
+  m = nimbleModel(code, data = list(y1 = rnorm(4), y2 = rnorm(4)),
+                  inits = list(xi = rep(1,4), thetatilde=rnorm(5)))
+  conf <- configureMCMC(m)
+  expect_match(conf$getSamplers()[[1]]$name, "dCRP_nonconjugate",
+               info = "failed to detect non conjugacy")
+  
+  code = nimbleCode({
+    for(i in 1:4)
+      y[i] ~ dnorm(thetatilde[xi[i]] + mu, sd = 1)
+    xi[1:4] ~ dCRP(1)
+    for(j in 1:4)
+      thetatilde[j] ~ dnorm(0,1)
+    mu ~ dnorm(0,1)
+  })
+  m = nimbleModel(code, data = list(y = rnorm(4)),
+                  inits = list(xi = rep(1,4), thetatilde=rnorm(4), mu=rnorm(1)))
+  conf <- configureMCMC(m)
+  expect_match(conf$getSamplers()[[1]]$name, "dCRP_nonconjugate",
+               info = "failed to detect non conjugacy")
+  
+  code = nimbleCode({
+    for(i in 1:4){
+      theta[i] <- thetatilde[xi[i]]
+      y[i] ~ dnorm(theta[i], sd = 1) }
+    xi[1:4] ~ dCRP(1)
+    for(j in 1:4)
+      thetatilde[j] ~ dnorm(0,1)
+  })
+  m = nimbleModel(code, data = list(y = rnorm(4)),
+                  inits = list(xi = rep(1,4), thetatilde=rnorm(4)))
+  conf <- configureMCMC(m)
+  expect_match(conf$getSamplers()[[1]]$name, "dCRP_conjugate_dnorm_dnorm",
+               info = "failed to detect normal normal conjugacy with determ node")
+  
+  code = nimbleCode({
+    for(i in 1:4){
+      theta[i] <- thetatilde[xi[i]]
+      y[i] ~ dnorm(0, var=theta[i]) }
+    xi[1:4] ~ dCRP(1)
+    for(j in 1:4)
+      thetatilde[j] ~ dinvgamma(1,1)
+  })
+  m = nimbleModel(code, data = list(y = rnorm(4)),
+                  inits = list(xi = rep(1,4), thetatilde=rinvgamma(4,1,1)))
+  conf <- configureMCMC(m)
+  expect_match(conf$getSamplers()[[1]]$name, "dCRP_nonconjugate",
+               info = "failed to detect non conjugacy for variance") # nos included yet
+  
+})
+
+
+
+
+
+
+######################################################################
+######################################################################
+
+# testing models
+
+testBUGSmodel(example = 'test', dir = "",
+              model = model, data = data, inits = inits,
+              useInits = TRUE)
+
+## simple test of normal normal DPM model with no deterministic nodes:
+
+model <- function() {
+  for(i in 1:3)
+    thetatilde[i] ~ dnorm(mean=0, var=1) 
+  xi[1:3] ~ dCRP(1)
+  for(i in 1:3)
+    y[i] ~ dnorm(thetatilde[xi[i]] , var=1)
+}
+
+inits <- list(thetatilde=rnorm(3), xi=rep(1,3))
+data <- list(y = rnorm(3))
+
+testBUGSmodel(example = 'testt', dir = "",
+              model = model, data = data, inits = inits,
+              useInits = TRUE)
 
