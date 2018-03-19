ed84019264ebdd95263d39a705ed82e49498e054
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2018-03-19 10:17:47 -0700

    Chris edits to BNP_distributions.R

--- packages/nimble/R/BNP_distributions.R
+++ packages/nimble/R/BNP_distributions.R
@@ -1,6 +1,6 @@
 
 #----------------------------------------------#
-# Chinesse restaurant process distributions:
+# Chinese restaurant process distributions:
 #----------------------------------------------#
 
 # random number generator: rCRP
@@ -22,24 +22,22 @@
 #   output:
 #      x: a value
 
-#' The Chinesse Restaurant Process Distribution
+#' The Chinese Restaurant Process Distribution
 #'
-#'   Density and random generation for the Chinesse
-#'   Restaurant Process distribution with concentration 
-#'   parameter \code{conc}.
+#'   Density and random generation for the Chinese
+#'   Restaurant Process distribution
 #' 
-#' @name ChinesseRestaurantProcess 
+#' @name ChineseRestaurantProcess 
 #' 
 #' @param x vector of values.
 #' @param n number of observations (only n = 1 is handled currently).
-#' @param conc scalar with concentration parameter.
-#' @param size integer of length of vector x.
+#' @param conc scalar concentration parameter.
+#' @param size integer-valued length of \code{x}.
 #' @param log logical; if TRUE, probability density is returned on the log scale.
 #' @author Claudia Wehrhahn
-#' @details The chineese restaurant process distribution is a distribution
+#' @details The Chinese restaurant process distribution is a distribution
 #' on the space of partitions of the positive integers. 
-#' The chineese restaurant process
-#' distribution with concentration parameter \eqn{=\alpha}{= conc} has 
+#' The distribution with concentration parameter \eqn{=\alpha}{= conc} has 
 #' probability function 
 #' \deqn{f(x_i \mid x_1, \ldots, x_{i-1})=\frac{1}{i-1+\alpha}\sum_{j=1}^{i-1}\delta_{x_j}+
 #' \frac{\alpha}{i-1+\alpha}\delta_{x^{new}},}
@@ -47,63 +45,60 @@
 #' 
 #' If \code{conc} is not specified, it assumes the default value of 1. 
 #' @return \code{dCRP} gives the density, and \code{rCRP} gives random generation.
-#' @references Blackwell, D., & MacQueen, J. B. (1973) Ferguson distributions via 
-#' Pólya urn schemes \emph{The annals of statistics}, 353-355.
+#' @references Blackwell, D., and MacQueen, J. B. (1973). Ferguson distributions via 
+#' Pólya urn schemes. \emph{The Annals of Statistics}, 1: 353-355.
 #' 
-#' Aldous, D. J. (1985) Exchangeability and related topics \emph{In École d'Été de 
-#' Probabilités de Saint-Flour XIII} 1983 (pp. 1-198) Springer, Berlin, Heidelberg.
+#' Aldous, D. J. (1985). Exchangeability and related topics. \emph{In École d'Été de 
+#' Probabilités de Saint-Flour XIII -- 1983}, Pages 1-198. Springer.
 #' 
-#' Pitman, Jim. Some developments of the Blackwell-MacQueen urn scheme 
-#' \emph{Statistics, probability and game theory}, 245--267, Institute of
-#'  Mathematical Statistics, Hayward, CA, 1996.
+#' Pitman, Jim. Some developments of the Blackwell-MacQueen urn scheme (1986). 
+#' \emph{Statistics, Probability and Game Theory: Papers in Honor of David Blackwell},
+#' 30: 245--267.
 #' 
 #' @examples
 #' x <- rCRP(n=1, conc = 1, size=10)
 #' dCRP(x, conc = 1, size=10)
 NULL
 
-#' @rdname ChinesseRestaurantProcess
+#' @rdname ChineseRestaurantProcess
 #' @export
-rCRP=nimbleFunction(
-  run=function(n = integer(0), 
-               conc = double(0, default=1),
-               size = integer(0))
-  {
-    returnType(double(1))
-    
-    if(n>1){
-      print(paste('Warning message: In rCRP(', n, ',', conc, ',', size, ') : rCRP only handles n = 1 at the moment'))
-    }
-    
-    if( conc <= 0 ){
-      concCond1 <- 1
-      print('conc parameter has to be larger than zero')
-    }else{
-      concCond1 <- 0
-    }
-    
-    if(concCond1 == 1){
-      return(c(NaN))
-    }else{
-      x <- numeric(size)# returned vector:
-      x[1] <- 1
-      if(size>1){
-        for(i in 2:size){
-          if( runif(1)<=conc/(conc+i-1) ){# a new value
-            x[i] <- max(x[1:(i-1)])+1 
-          }else{# an old value
-            index <- rcat(n=1, rep(1, i-1)) 
-            x[i] <- x[index]
-          }
+rCRP <- nimbleFunction(
+    run = function(n = integer(0), 
+                   conc = double(0, default=1),
+                   size = integer(0))
+    {
+        returnType(double(1))
+        
+        if(n != 1){
+            stop("rCRP only handles n = 1 at the moment")
         }
-      }
-      return(x)
+        
+        if( conc <= 0 ){
+            warning("value of concentration parameter has to be larger than zero")
+            return(rep(NaN, size))
+        }
+
+        x <- nimNumeric(size) 
+        x[1] <- 1
+        if(size > 1){
+            numComponents <- 1
+            ones <- rep(1, size)
+            for(i in 2:size) {
+                if(runif(1) <= conc / (conc+i-1) ) {  # a new value
+                    numComponents <- numComponents + 1
+                    x[i] <- numComponents
+                } else {                             # an old value
+                    index <- rcat(n=1, ones[1:i-1])
+                    x[i] <- x[index]
+                }
+            }
+        }
+        return(x)
     }
-  }
 )
 
 
-#' @rdname ChinesseRestaurantProcess
+#' @rdname ChineseRestaurantProcess
 #' @export
 dCRP=nimbleFunction(
   run=function(x = double(1), 
@@ -113,39 +108,27 @@ dCRP=nimbleFunction(
   {
     returnType(double(0))
     n <- length(x) 
-    tmpden <- numeric(n) 
+    dens <- nimNumeric(n) 
     
     if( conc <= 0 ){
-      concCond1 <- 1
-      print('conc parameter has to be larger than zero')
-    }else{
-      concCond1 <- 0
+        warning("value of concentration parameter has to be larger than zero")
+        return(rep(NaN, size))
     }
-    
-    if(concCond1 == 1){
-      return(NaN)
-    }else{
-      tmpden[1] <- 1
-      if(n>1){
+    dens[1] <- 1
+    if(n > 1){
         for(i in 2:n){
-          #counts=0 # replaces sum(x[i]==x[1:(i-1)]) (works in nimble?)
-          #for(j in 1:(i-1)){
-          #  if(x[i]==x[j]){
-          #    counts=counts+1
-          #  }
-          #}
-          counts <- sum(x[i]==x[1:(i-1)])
-          if( counts>0 ){
-            tmpden[i] <- 1/(i-1+conc)
-          }else{
-            tmpden[i] <- conc/(i-1+conc)
-          }
+            counts <- sum(x[i] == x[1:(i-1)])
+            if( counts > 0 ){
+                ## Claudia: shouldn't this be 'counts / (i-1+conc)' ???
+                dens[i] <- 1 / (i-1+conc)
+            } else {
+                dens[i] <- conc / (i-1+conc)
+            }
         }
-      }
-      logProb <- sum(log(tmpden))
-      if(log) return(logProb)
-      else return(exp(logProb)) 
     }
+    logProb <- sum(log(dens))
+    if(log) return(logProb)
+    else return(exp(logProb)) 
   }
 )
 
@@ -159,23 +142,28 @@ dCRP=nimbleFunction(
 # stick_breaking nimbleFunction
 #----------------------------------------------#
 
-#' The Stick Breaking function
+#' The Stick Breaking Function
 #'
-#'   Based on the stick breaking construction, weights are computed 
-#'   with the argument vector.
+#' Computes probabilities based on stick breaking construction
 #' 
 #' @name StickBreakingFunction 
 #' 
 #' @param z vector argument.
 #' @param log logical; if TRUE, weights are returned on the log scale.
 #' @author Claudia Wehrhahn
-#' @details values in \code{z} have to be between \deqn{[0,1)]}. If one of the components 
+#' @details
+#' The stick breaking construction produces a vector of probabilities that sums to one,
+#' based on a series of individual probabilities in \code{z} that have to be between \deqn{[0,1)]}.
+#' The construction starts by breaking a piece of stick off of a stick of length one, based on
+#' \code{z[1]}. The first element the output is the length of the piece that was broken off.
+#' The construction then proceeds by breaking a piece of stick off of the remaining stick, based on
+#' \code{z[2]} and so forth. If \code{z[k]}
 #' is equal to 1, then the returned vector has length smaller than \code{z}. If one of the
 #' components is smaller than 0 or greater than 1, NaNs are returned.
-#' @references Sethuraman, J. (1994) A constructive definition of Dirichlet 
-#' priors. \emph{Statistica sinica}, 639-650.
+#' @references Sethuraman, J. (1994). A constructive definition of Dirichlet 
+#' priors. \emph{Statistica Sinica}, 4: 639-650.
 #' @examples
-#' z <- rbeta(5, 1,1)
+#' z <- rbeta(5, 1, 1)
 #' stick_breaking(z)
 #' 
 #' cstick_breaking <- compileNimble(stick_breaking)
@@ -184,53 +172,47 @@ NULL
 
 #' @rdname StickBreakingFunction
 #' @export
-stick_breaking=nimbleFunction(
-  run=function(z=double(1),
-               log=integer(0, default=0)){ # z values must be different of 1, otherwise the process is truncated to a smaller value tha N; never use z[N]!
-    returnType(double(1))
-    
-    cond <- sum(z < 0)
-    if(cond > 0){
-      print('values in vector z have to be in [0,1)')
-      cond1 <- 1
-    }else{
-      cond1 <- 0
+stick_breaking <- nimbleFunction(
+    run = function(z = double(1),
+                   log = integer(0, default=0)) {
+        ## z values must be different of 1, otherwise the process is truncated to a smaller value than N; never use z[N]!
+        returnType(double(1))
+
+        ## Claudia: should we require z to be length of output? The main issue is that then there needs to be an unused (last) 'z' that is just sampled from its prior... but maybe it's less confusing to have z and the output be the same length?
+        
+        N <- length(z)
+        cond <- sum(z < 0) | sum(z > 1)
+        if(cond) {
+            warning("values in 'z' have to be in [0,1)")
+            return(rep(NaN, N))
+        }
+        
+        cond <- sum(z == 1)
+        if(cond > 0){ # el vector de probabilidades es mas chico
+            ## Claudia: I don't understand why we need this block of code; if a 'z' value is 1, then won't the code below (corrrectly) insert zeros for all the remaining elements?
+            ## In particular, I think if we return a shorter vector that will cause errors later in nimble calculations where the model expects a vector of length N.
+            ## If we alter this, then make sure to modify the roxygen regardin the length of N
+            print('length of returned vector is less than length(z)')
+            N <- 1
+            while(z[N] != 1){
+                N <- N + 1
+            }
+        }else{
+            N <- length(z)
+        }
+        
+        x <- nimNumeric(N) 
+        remainingLogProb <- 0 
+        
+        x[1] <- log(z[1]) 
+        for(i in 2:(N-1)) {
+            remainingLogProb <- remainingLogProb + log(1-z[i-1]) 
+            x[i] <- log(z[i]) + remainingLogProb 
+        }
+        x[N] <- remainingLogProb + log(1-z[N-1]) 
+        if(log) return(x)
+        else return(exp(x))
     }
-    cond <- sum(z > 1)
-    if(cond > 0){
-      print('values in vector z have to be in [0,1)')
-      cond2 <- 1
-    }else{
-      cond2 <- 0
-    }
-    
-    cond <- sum(z == 1)
-    if(cond > 0){ # el vector de probabilidades es mas chico
-      print('length of returned vector is less than length(z)')
-      N <- 1
-      while(z[N] != 1){
-        N <- N + 1
-      }
-    }else{
-      N<-length(z)
-    }
-    
-    if(cond1 + cond2 > 0){
-      return(c(NaN))
-    }else{
-      x<-numeric(N) # returned vector
-      tmp<-0#1
-      
-      x[1]<-log(z[1]) #z[1]
-      for(i in 2:(N-1)){
-        tmp=tmp+log(1-z[i-1]) #tmp*(1-z[i-1])
-        x[i]<-log(z[i])+tmp #z[i]*tmp
-      }
-      x[N]<-tmp+log(1-z[N-1])#tmp*(1-z[N-1])
-      if(log) return(x)
-      else return(exp(x))
-    }
-  }
 )
 
 
