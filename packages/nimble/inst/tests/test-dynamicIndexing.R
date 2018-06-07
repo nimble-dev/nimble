ea4483c35ea5ade3fad5667a5775c440ed8a8ce0
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2018-06-07 08:28:22 -0700

    add seed to new dyn idx test

--- packages/nimble/inst/tests/test-dynamicIndexing.R
+++ packages/nimble/inst/tests/test-dynamicIndexing.R
@@ -296,6 +296,7 @@ test_that("MCMC with invalid indexes produce warning, but runs", {
     cm <- compileNimble(m)
     mcmc = buildMCMC(m)
     cmcmc = compileNimble(mcmc ,project=m)
+    set.seed(1)
     expect_output(cmcmc$run(10000), "dynamic index out of bounds")
     out <- as.matrix(cmcmc$mvSamples)
     expect_equal(sum(out == 2) / sum(out == 3), dnorm(0)/dnorm(1), tolerance = .02)
