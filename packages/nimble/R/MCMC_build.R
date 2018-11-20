fac5aa77ca4dd98bcc05028b5f2b36af55e71e6a
danielturek <danielturek@gmail.com>
2018-11-20 16:22:30 -0500

    undid changes to tr travis testing again

--- packages/nimble/R/MCMC_build.R
+++ packages/nimble/R/MCMC_build.R
@@ -240,7 +240,7 @@ buildMCMC <- nimbleFunction(
             logPredProbs <- matrix(nrow = numMCMCSamples, ncol = dataNodeLength)
             logAvgProb <- 0
             pWAIC <- 0
-            currentVals <- values(model)
+            currentVals <- values(model, sampledNodes)
             for(i in 1:numMCMCSamples) {
                 copy(mvSamples, model, nodesTo = sampledNodes, row = i + nburninPostThinning)
                 model$simulate(paramDeps)
@@ -256,8 +256,8 @@ buildMCMC <- nimbleFunction(
                 pWAIC <- pWAIC + pointLogPredVar
             }
             WAIC <- -2*(logAvgProb - pWAIC)
-            values(model) <<- currentVals
-            model$calculate()
+            values(model, sampledNodes) <<- currentVals
+            model$calculate(paramDeps)
             returnType(double())
             return(WAIC)
         }),
