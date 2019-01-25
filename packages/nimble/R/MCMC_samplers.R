4fac54097de2261da2a6aae04307fb78545a3e28
perrydv <pdevalpine@berkeley.edu>
2019-01-24 17:21:27 -0800

    Revert "make sampler_categorical and sampler_binary separate logProb prior from dependencies"
    This reverts commit bd68b43b54d6b166dcee97e3f1faed48c75a28ad.

--- packages/nimble/R/MCMC_samplers.R
+++ packages/nimble/R/MCMC_samplers.R
@@ -53,7 +53,6 @@ sampler_binary <- nimbleFunction(
         ## node list generation
         targetAsScalar <- model$expandNodeNames(target, returnScalarComponents = TRUE)
         calcNodes <- model$getDependencies(target)
-        calcNodesNoSelf <- model$getDependencies(target, self = FALSE)
         ## checks
         if(length(targetAsScalar) > 1)  stop('cannot use binary sampler on more than one target node')
         if(!model$isBinary(target))     stop('can only use binary sampler on discrete 0/1 (binary) nodes')
@@ -61,12 +60,7 @@ sampler_binary <- nimbleFunction(
     run = function() {
         currentLogProb <- getLogProb(model, calcNodes)
         model[[target]] <<- 1 - model[[target]]
-        otherLogProbPrior <- calculate(model, target)
-        if(otherLogProbPrior == -Inf) {
-            otherLogProb <- otherLogProbPrior
-        } else {
-            otherLogProb <- otherLogProbPrior + calculate(model, calcNodesNoSelf)
-        }
+        otherLogProb <- calculate(model, calcNodes)
         acceptanceProb <- 1/(exp(currentLogProb - otherLogProb) + 1)
         if(!is.nan(acceptanceProb) & runif(1,0,1) < acceptanceProb)
             nimCopy(from = model, to = mvSaved, row = 1, nodes = calcNodes, logProb = TRUE)
@@ -93,7 +87,6 @@ sampler_categorical <- nimbleFunction(
         ## node list generation
         targetAsScalar <- model$expandNodeNames(target, returnScalarComponents = TRUE)
         calcNodes  <- model$getDependencies(target)
-        calcNodesNoSelf <- model$getDependencies(target, self = FALSE)
         ## numeric value generation
         k <- length(model$getParam(target, 'prob'))
         probs <- numeric(k)
@@ -107,17 +100,8 @@ sampler_categorical <- nimbleFunction(
         for(i in 1:k) {
             if(i != currentValue) {
                 model[[target]] <<- i
-                logProbPrior <- calculate(model, target)
-                if(logProbPrior == -Inf) {
-                    probs[i] <<- 0
-                } else {
-                    if(is.nan(logProbPrior)) {
-                        probs[i] <<- 0
-                    } else {
-                        probs[i] <<- exp(logProbPrior + calculate(model, calcNodesNoSelf))
-                        if(is.nan(probs[i])) probs[i] <<- 0
-                    }
-                }
+                probs[i] <<- exp(calculate(model, calcNodes))
+                if(is.nan(probs[i])) probs[i] <<- 0
             }
         }
         newValue <- rcat(1, probs)
