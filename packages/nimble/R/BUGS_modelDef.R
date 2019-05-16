97c2b974f7e5ec00158b165e747b3fef505fb40c
perrydv <pdevalpine@berkeley.edu>
2019-05-15 17:42:45 -0700

    rewrite of uncompiled nimDerivs
    This required fixing model$expandNodeNames when returnScalarComponents = TRUE and unique = TRUE.  This appears to be the first time that combination has been needed anywhere.

--- packages/nimble/R/BUGS_modelDef.R
+++ packages/nimble/R/BUGS_modelDef.R
@@ -2836,7 +2836,10 @@ modelDefClass$methods(nodeName2GraphIDs = function(nodeName, nodeFunctionID = TR
         else
             output2 <- unlist(lapply(parseEvalNumericManyList(nodeName, env = maps$vars2GraphID_functions_and_RHSonly), unique))
     } else {
-        output2 <- unique(parseEvalNumericMany(nodeName, env = maps$vars2ID_elements))
+        if(unique)
+            output2 <- unique(parseEvalNumericMany(nodeName, env = maps$vars2ID_elements))
+        else
+            output2 <- parseEvalNumericMany(nodeName, env = maps$vars2ID_elements)
     }
     output <- output2
     return(output[!is.na(output)])
