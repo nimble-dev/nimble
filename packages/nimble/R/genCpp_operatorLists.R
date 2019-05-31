04da6339cdd18eae545d05e62f03868c9f7e2cfb
James Duncan <jpduncan@berkeley.edu>
2019-05-30 19:16:13 -0700

    Update recyclingRuleOperatorsAD to reflect what is supported and working

--- packages/nimble/R/genCpp_operatorLists.R
+++ packages/nimble/R/genCpp_operatorLists.R
@@ -339,10 +339,10 @@ nimDerivsPrependTypeOperators <- c("dnorm", "dpois", "dgamma", "dinvgamma", "dex
                                    "nimStep", 'ilogit', 'icloglog', 'iprobit', 'probit', 'cloglog',
                                    "nimEquals", "lgammafn", "gammafn", "lfactorial", "factorial", "logit", "cube", "inprod")
 
-## reflects distribution funs that support AD
+## reflects distribution funs that support AD, see nimDerivs_TMB.h
 recyclingRuleOperatorsAD <- c(
-  scalar_distribution_dFuns,
-  'pnorm', 'qnorm'
+  'dbinom', 'dexp_nimble', 'dnbinom', 'dpois', 'dchisq', 'dbeta', 'dnorm', 'dgamma',
+  'dinvgamma', 'ddexp', 'dlnorm', 'dlogis', 'dunif', 'dweibull'
 )
 recyclingRuleOperatorsAD <- paste0(
     recyclingRuleOperatorsAD,
