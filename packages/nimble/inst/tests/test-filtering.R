95f2901b6351a60f397d8e12f5123250d10082cd
Christopher Paciorek <paciorek@stat.berkeley.edu>
2019-11-10 16:09:25 -0800

    modify test_filtering to have deterministic intermediate nodes for various cases

--- packages/nimble/inst/tests/test-filtering.R
+++ packages/nimble/inst/tests/test-filtering.R
@@ -28,15 +28,15 @@ nimbleOptions(MCMCprogressBar = FALSE)
 
 code <- nimbleCode({
   x0 ~ dnorm(0,1)
-  x[1] ~ dnorm(x0, 1)
+  x[1] ~ dnorm(a + b*x0, 1)
   y[1] ~ dnorm(x[1], var = 2)
     for(i in 2:3) {
-        x[i] ~ dnorm(x[i-1], 1)
+        x[i] ~ dnorm(a + b*x[i-1], 1)
         y[i] ~ dnorm(x[i], var = 2)
     }
 })
 testdata = list(y = c(0,1,2))
-inits = list(x0 = 0)
+inits = list(x0 = 0, a = 0, b = 1)
 
 ### ll, means, vars calculated from FKF R packgae
 ActualLL <- -5.08
@@ -60,7 +60,6 @@ test_filter(model = code, name = 'basic auxiliary', data = testdata, filterType 
                                      var = list(x = rep(.2,3)),
                                      ll = list(2)))
 
-if(FALSE) { ## add this and adjust gold file
 test_filter(model = code, name = 'basic auxiliary, saveAll = FALSE', data = testdata, filterType = "auxiliary", latentNodes = "x", filterControl = list(saveAll = FALSE), 
              inits = inits,
              results = list(mean = list(x = c(0,0.454,1.209)),
@@ -69,7 +68,7 @@ test_filter(model = code, name = 'basic auxiliary, saveAll = FALSE', data = test
              resultsTolerance = list(mean = list(x = rep(.2,3)),
                                      var = list(x = rep(.2,3)),
                                      ll = list(2)))
-}
+
 
 test_filter(model = code, name = 'basic auxiliary w/ mean lookahead', data = testdata, filterType = "auxiliary",
              latentNodes = "x", filterControl = list(lookahead = "mean", saveAll = TRUE),
@@ -97,7 +96,7 @@ code <- nimbleCode({
   yMean[1,1:2] <- obsMat[1:2,1:3]%*%x[1,1:3]
   y[1,1:2] ~ dmnorm(yMean[1,1:2], cov = yCov[1:2,1:2])
   for(i in 2:3) {
-    prevX[i,1:3] <- x[i-1,1:3]
+    prevX[i,1:3] <- a[1:3] + b * x[i-1,1:3]
     x[i,1:3] ~ dmnorm(prevX[i,1:3] , cov = xCov[1:3, 1:3])
     yMean[i,1:2] <- obsMat[1:2,1:3]%*%x[i,1:3]
     y[i,1:2] ~  dmnorm(yMean[i,1:2], cov = yCov[1:2,1:2])
@@ -124,13 +123,13 @@ xFilterTol <- matrix(1.2, nrow = 3, ncol = 3)
 ActualLL <- -10.235
 
 
-test_filter(model = code, name = 'multivariate bootstrap, always resamp', data = testdata, filterType = "bootstrap", latentNodes = "x",
+test_filter(model = code, name = 'multivariate bootstrap, always resamp', data = testdata, filterType = "bootstrap", latentNodes = "x", inits = list(a = rep(0,3), b = 1), 
              filterControl = list(thresh = 1, saveAll = TRUE, timeIndex = 1),
              results = list(mean = list(x = xFilter),
                             ll = list(ActualLL)),
              resultsTolerance = list(mean = list(x = xFilterTol),
                                       ll = list(2)))
-test_filter(model = code, name = 'multivariate auxiliary', data = testdata, filterType = "auxiliary", latentNodes = "x",
+test_filter(model = code, name = 'multivariate auxiliary', data = testdata, filterType = "auxiliary", latentNodes = "x", inits = list(a = rep(0,3), b = 1), 
              filterControl = list(saveAll = TRUE, timeIndex = 1),
              results = list(mean = list(x = xFilter),
                             ll = list(ActualLL)),
@@ -145,14 +144,14 @@ if(.Platform$OS.type == 'windows') {
     stop()
 }
 
-test_filter(model = code, name = 'multivariate auxiliary mean lookahead', data = testdata, filterType = "auxiliary", latentNodes = "x",
+test_filter(model = code, name = 'multivariate auxiliary mean lookahead', data = testdata, filterType = "auxiliary", latentNodes = "x", inits = list(a = rep(0,3), b = 1),
              filterControl = list(saveAll = TRUE, timeIndex = 1, lookahead = "mean"),
              results = list(mean = list(x = xFilter),
                             ll = list(ActualLL)),
              resultsTolerance = list(mean = list(x = xFilterTol),
                                      ll = list(2)))
 
-test_filter(model = code, name = 'multivariate ensembleKF', data = testdata, filterType = "ensembleKF", latentNodes = "x",
+test_filter(model = code, name = 'multivariate ensembleKF', data = testdata, filterType = "ensembleKF", latentNodes = "x", inits = list(a = rep(0,3), b = 1),
             filterControl = list(timeIndex = 1, saveAll = F),
             results = list(mean = list(x = xFilter[3,])),
             resultsTolerance = list(mean = list(x = xFilterTol[3,])))
@@ -164,7 +163,7 @@ code <- nimbleCode({
   x[1] ~ dnorm(mean = mu0, sd = sigma_x);
   y[1] ~ dnorm(x[1], sd=sigma_y);
   for(i in 2:N){
-    x[i] ~ dnorm(mean = x[i-1], sd = sigma_x);
+    x[i] ~ dnorm(mean = a + b*x[i-1], sd = sigma_x);
     y[i] ~ dnorm(mean = x[i], sd=sigma_y);
   }
   sigma_x ~ T(dnorm(1, sd = .5), 0,);
@@ -174,6 +173,8 @@ code <- nimbleCode({
 
 set.seed(0)
 
+a <- 0
+b <- 1
 N <- 5
 sigma_x <- 1
 sigma_y <- .1
@@ -182,7 +183,7 @@ y <- x
 x[1] <- rnorm(1,0,sigma_x)
 y[1] <- rnorm(1,x[1], sigma_y)
 for(i in 2:N){
-  x[i] <- rnorm(1,x[i-1], sigma_x)
+  x[i] <- rnorm(1,a+b*x[i-1], sigma_x)
   y[i] <- rnorm(1,x[i], sigma_y)
 }
 
@@ -190,8 +191,9 @@ consts <- list(N=N)
 
 testdata <- list(y=y)
 
-inits <- list(sigma_x=1, sigma_y=.1, x = x)
+inits <- list(sigma_x=sigma_x, sigma_y=sigma_y, x = x, a = a, b = b)
 
+##  this now fails with a+b*x offset because of issue #955
 test_filter(model = code, name = 'scalar lwf', inits = inits, data = c(testdata, consts), filterType = "LiuWest", latentNodes = "x", results = list(
   mean = list(x = x,
               sigma_x = sigma_x,
