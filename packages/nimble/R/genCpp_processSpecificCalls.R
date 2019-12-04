4ed8d6813cb13bd6f145c36865d16c091ef16020
Christopher Paciorek <paciorek@stat.berkeley.edu>
2019-12-04 08:13:27 -0800

    keep is.na{,n}.vec available for backwards compatibility

--- packages/nimble/R/genCpp_processSpecificCalls.R
+++ packages/nimble/R/genCpp_processSpecificCalls.R
@@ -13,8 +13,10 @@ specificCallReplacements <- list(
     length = 'size',
     is.nan = 'ISNAN',
     any_nan = 'ISNAN',
+    is.nan.vec = 'ISNAN',  ## retained for backward compatibility
     is.na = 'ISNA',
     any_na = 'ISNA',
+    is.na.vec = 'ISNA',    ## retained for backward compatibility
     lgamma = 'lgammafn',
     logfact = 'lfactorial',
     loggam = 'lgammafn',
