c1a494ac29cbeec88e8f7fb07d42b1ff3bcd0d6a
perrydv <pdevalpine@berkeley.edu>
2020-03-19 09:40:41 -0700

    draft extension to sizeRmultivarFirstArg

--- packages/nimble/R/genCpp_sizeProcessing.R
+++ packages/nimble/R/genCpp_sizeProcessing.R
@@ -3259,7 +3259,17 @@ mvFirstArgCheckLists <- list(nimArr_rmnorm_chol = list(c(1, 2, 0), ## dimensiona
                              nimArr_rmvt_chol = list(c(1, 2, 0, 0), ## dimensionality of ordered arguments AFTER the first, which is for the return value.  e.g. mean (1D), chol(2D), df(scalar), prec_param(scalar)
                                                        1, 'double'), ## 1 = argument from which to take answer size, double = answer type
                              nimArr_rlkj_corr_cholesky = list(c(0, 0), ## eta, p
-                                 1, 'double'),  # '1' won't work here; problem is that no matrices are input but matrix is output
+                                                              function(code) {
+                                                                  ## example
+                                                                  ## Rcode <- quote(nimArr_rlkj_corr_cholesky(eta = k * rho, p = k + a) )
+                                                                  ## code <- nimble:::RparseTree2ExprClasses(Rcode)
+                                                                  dimCode <- parse(text = nimDeparse(code$args[['p']]), keep.source = FALSE)[[1]]
+                                                                  sizeExprs <- list(dimCode, dimCode)
+                                                                  ## dimCode should be quote( k + a )
+                                                                  list(nDim = 2,
+                                                                       sizeExprs)
+                                                              },
+                                                              'double'),  # '1' won't work here; problem is that no matrices are input but matrix is output
                              nimArr_rwish_chol = list(c(2, 0, 0, 0), ## chol, df, prec_param, overwrite_inputs
                                  1, 'double'),
                              nimArr_rinvwish_chol = list(c(2, 0, 0), ## chol, df, prec_param
@@ -3291,13 +3301,20 @@ sizeRmultivarFirstArg <- function(code, symTab, typeEnv) {
         stop(exprClassProcessingErrorMsg(code, 'Some argument(s) have the wrong dimension.'), call. = FALSE) 
     }
 
-    if(!inherits(code$args[[returnSizeArgID]], 'exprClass')) stop(exprClassProcessingErrorMsg(code, paste0('Expected ', nimDeparse(code$args[[returnSizeArgID]]) ,' to be an expression.')), call. = FALSE) 
+    if(!(is.function(returnSizeArgID)))
+        if(!inherits(code$args[[returnSizeArgID]], 'exprClass'))
+            stop(exprClassProcessingErrorMsg(code, paste0('Expected ', nimDeparse(code$args[[returnSizeArgID]]) ,' to be an expression or function.')), call. = FALSE) 
     
     code$type <- returnType
-    code$nDim <- code$args[[returnSizeArgID]]$nDim
     code$toEigenize <- 'maybe'
-    code$sizeExprs <- code$args[[returnSizeArgID]]$sizeExprs
-    
+    if(!is.function(returnSizeArgID)) {
+        code$nDim <- code$args[[returnSizeArgID]]$nDim
+        code$sizeExprs <- code$args[[returnSizeArgID]]$sizeExprs
+    } else {
+        sizeInfo <- returnSizeArgID(code)
+        code$nDim <- sizeInfo$nDim
+        code$sizeExprs <- sizeInfo$sizeExprs
+    }
     for(i in seq_along(code$args)) {
         if(inherits(code$args[[i]], 'exprClass')) {
             if(!code$args[[i]]$isName) {
