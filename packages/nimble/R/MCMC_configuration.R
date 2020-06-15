cbf9e6d6425d334007641435707758f9edae06a2
Christopher Paciorek <paciorek@stat.berkeley.edu>
2020-06-15 14:57:41 -0700

    improve efficiency of BNP sampler setup:  - improve findClusterNodes efficiency  - call findClusterNodes in MCMC_configuration and pass into sampler_CRP  - use getParenNodes in sampler_CRP setup

--- packages/nimble/R/MCMC_configuration.R
+++ packages/nimble/R/MCMC_configuration.R
@@ -310,9 +310,10 @@ print: A logical argument specifying whether to print the ordered list of defaul
                         if(nodeDist == 'dcar_normal')  { addSampler(target = node, type = 'CAR_normal');         next }
                         if(nodeDist == 'dcar_proper')  { addSampler(target = node, type = 'CAR_proper');         next }
                         if(nodeDist == 'dCRP')         {
-                            addSampler(target = node, type = 'CRP', control = list(checkConjugacy = useConjugacy))
                             numCRPnodes <- numCRPnodes + 1
                             clusterNodeInfo[[numCRPnodes]] <- findClusterNodes(model, node)
+                            addSampler(target = node, type = 'CRP', control = list(checkConjugacy = useConjugacy,
+                                                                                   clusterVarInfo = clusterNodeInfo[[numCRPnodes]]))
                             dcrpNode[numCRPnodes] <- node
                             next
                         }
