6eebb9980a3d7a778dfd034489091b1ff6b837ad
Christopher Paciorek <paciorek@stat.berkeley.edu>
2021-02-17 13:02:28 -0800

    more debug why tests not installed with nimSMC

--- install_nimble_related.R
+++ install_nimble_related.R
@@ -5,8 +5,8 @@ requirements <- c(
 )     
 
 for (package in requirements) {
-    if (!suppressPackageStartupMessages(require(package,
+ #   if (!suppressPackageStartupMessages(require(package,
                                                 character.only = TRUE))) {
-        install.packages(package, repos = 'http://cran.us.r-project.org', INSTALL_opts = '--install-tests', verbose = TRUE, quiet = FALSE)
-    }
+ #       install.packages(package, repos = 'http://cran.us.r-project.org', INSTALL_opts = '--install-tests', verbose = TRUE, quiet = FALSE)
+ #   }
 }
