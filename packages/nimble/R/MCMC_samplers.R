334f30a83bfecabfae5d8db83cc77973ad0690e7
Christopher Paciorek <paciorek@stat.berkeley.edu>
2021-06-07 12:57:49 -0700

    partial draft of block lkj sampler

--- packages/nimble/R/MCMC_samplers.R
+++ packages/nimble/R/MCMC_samplers.R
@@ -1879,6 +1879,222 @@ sampler_RW_lkj_corr_cholesky <- nimbleFunction(
     ), where = getLoadingNamespace()
 )
 
+############################################################################################
+### RW_block_lkj_corr_cholesky sampler for non-conjugate LKJ correlation Chlesky factor  ###
+############################################################################################
+
+
+#' @rdname samplers
+#' @export
+sampler_RW_block_lkj_corr_cholesky <- nimbleFunction(
+    name = 'sampler_RW_block_lkj_corr_cholesky',
+    contains = sampler_BASE,
+    setup = function(model, mvSaved, target, control) {
+        ## control list extraction
+        adaptive            <- if(!is.null(control$adaptive))            control$adaptive            else TRUE
+        adaptScaleOnly      <- if(!is.null(control$adaptScaleOnly))      control$adaptScaleOnly      else FALSE
+        adaptInterval       <- if(!is.null(control$adaptInterval))       control$adaptInterval       else 200
+        adaptFactorExponent <- if(!is.null(control$adaptFactorExponent)) control$adaptFactorExponent else 0.8
+        scale               <- if(!is.null(control$scale))               control$scale               else 1
+        propCov             <- if(!is.null(control$propCov))             control$propCov             else 'identity'
+        tries               <- if(!is.null(control$tries))               control$tries               else 1
+        ## node list generation
+        target <- model$expandNodeNames(target)
+        targetAsScalar <- model$expandNodeNames(target, returnScalarComponents = TRUE)
+        calcNodes <- model$getDependencies(target)
+        finalTargetIndex <- max(match(model$expandNodeNames(target), calcNodes))
+        if(!is.integer(finalTargetIndex) |
+           length(finalTargetIndex) != 1 |
+           is.na(finalTargetIndex[1]))
+            stop('Problem with target node in sampler_RW_block_lkj_corr_cholesky')
+        calcNodesProposalStage <- calcNodes[1:finalTargetIndex]
+        calcNodesDepStage <- calcNodes[-(1:finalTargetIndex)]
+#        calcNodesNoSelf <- model$getDependencies(target, self = FALSE)
+        ## numeric value generation
+        scaleOriginal <- scale
+        timesRan      <- 0
+        timesAccepted <- 0
+        timesAdapted  <- 0
+        p <- sqrt(length(targetAsScalar))
+        d <- p*(p-1)/2    # this many unconstrained elements to be sampled
+        scaleHistory  <- c(0, 0)                                                 ## scaleHistory
+        acceptanceHistory  <- c(0, 0)                                            ## scaleHistory
+        propCovHistory <- if(d<=10) array(0, c(2,d,d)) else array(0, c(2,2,2))   ## scaleHistory
+        saveMCMChistory <- if(nimbleOptions('MCMCsaveHistory')) TRUE else FALSE
+        if(is.character(propCov) && propCov == 'identity')     propCov <- diag(d)
+        propCovOriginal <- propCov
+        chol_propCov <- chol(propCov)
+        chol_propCov_scale <- scale * chol_propCov
+        empirSamp <- matrix(0, nrow=adaptInterval, ncol=d)
+        ## nested function and function list definitions
+        ##        my_setAndCalculateDiff <- setAndCalculateDiff(model, target)
+        targetNodesAsScalar <- model$expandNodeNames(target, returnScalarComponents = TRUE)
+##        my_decideAndJump <- decideAndJump(model, mvSaved, calcNodes)
+        my_calcAdaptationFactor <- calcAdaptationFactor(d, adaptFactorExponent)
+
+        z                   <- array(0, c(p, p))
+        diag(z)             <- 1
+        y                   <- numeric(d)
+        partialSums         <- array(0, c(p, p))  # 1-x_{21}^2, 1-x_{31}^2, 1-x_{31}^2-x_{32}^2, ...
+        partialSums[1, ]    <- 1
+        logSum              <- 0
+        
+        ## checks
+        if(class(propCov) != 'matrix')        stop('propCov must be a matrix\n')
+        if(class(propCov[1,1]) != 'numeric')  stop('propCov matrix must be numeric\n')
+        if(!all(dim(propCov) == d))           stop('propCov matrix must have dimension ', d, 'x', d, '\n')
+        if(!isSymmetric(propCov))             stop('propCov matrix must be symmetric')
+
+        dist <- model$getDistribution(target)
+        if(dist != 'dlkj_corr_cholesky') stop('RW_block_lkj_corr_cholesky sampler can only be used with the dlkj_corr_cholesky distribution.')
+        if(d < 2)                        stop('RW_block_lkj_corr_cholesky sampler requires target node dimension to be at least 2x2.')
+        if(adaptFactorExponent < 0)      stop('Cannot use RW_block_lkj_corr_cholesky sampler with adaptFactorExponent control parameter less than 0.')
+
+    },
+    run = function() {
+        transform(model[[target]])  # compute z and partialSums
+        for(k in 1:tries) {
+            yPropValueVector <- generateProposalVector()
+            ##        lpMHR <- my_setAndCalculateDiff$run(propValueVector)
+            cnt <- 1
+            for(i in 2:d) {
+                model[[target]][1, i] <- tanh(yPropValueVector[cnt])
+                logMHR <- logMHR + 2*(log(cosh(y[cnt])) - log(cosh(yPropValueVector[cnt])))
+                cnt <- cnt+1
+                partialSums <- 1 - model[[target]][1, i]^2
+                if(i > 2) {
+                    for(j in 2:(i-1)) {
+                        model[[target]][j, i] <<- tanh(yPropValueVector[cnt]) * sqrt(partialSums)
+                        logMHR <- logMHR + 2*(log(cosh(y[cnt])) - log(cosh(yPropValueVector[cnt]))) +
+                            0.5*log(partialSums)
+                        partialSums <- partialSums - model[[target]][j, i]^2
+                        cnt <- cnt+1
+                    }
+                }
+            }
+    
+            logMHR <- logMHR - 0.5*logSum
+
+            values(model, targetNodesAsScalar) <<- propValueVector
+            lpD <- calculateDiff(model, calcNodesProposalStage)
+            if(lpD == -Inf) {
+                nimCopy(from = mvSaved, to = model,   row = 1, nodes = calcNodesProposalStage, logProb = TRUE)
+                jump <- FALSE
+            } else {
+                ##        jump <- my_decideAndJump$run(lpMHR, 0, 0, 0) ## will use lpMHR - 0
+                lpD <- lpD + calculateDiff(model, calcNodesDepStage)
+                jump <- decide(lpD)
+                if(jump) { nimCopy(from = model,   to = mvSaved, row = 1, nodes = calcNodes, logProb = TRUE)
+                } else   { nimCopy(from = mvSaved, to = model,   row = 1, nodes = calcNodes, logProb = TRUE) }
+            }
+            if(adaptive)     adaptiveProcedure(jump)
+        }
+    },
+    methods = list(
+        transform = function(x = double(2)) {
+            z[1, 2:d] <<- x[1, 2:d]
+            y[1] <<- atanh(z[1, 2])
+            cnt <- 2
+            partialSums[2, 2] <<- 1 - x[1, 2]^2
+            for(i in 3:d) {
+                y[cnt] <<- atanh(z[1, i])
+                cnt <- cnt+1
+                for(j in 2:(i-1)) {
+                    logSum <<- log(partialSums[j-1 , i])
+                    partialSums[j, i] <<- partialSums[j-1, i] - x[j-1, i]^2
+                    z[j, i] <<- x[j, i] / sqrt(partialSums[j, i])
+                    y[cnt] <<- atanh(z[j, i])
+                    cnt <- cnt+1
+                }
+                partialSums[i, i] <<- partialSums[i-1, i] - x[i-1, i]^2
+            }
+        },
+        generateProposalVector = function() {
+            propValueVector <- rmnorm_chol(1, y, chol_propCov_scale, 0)  ## last argument specifies prec_param = FALSE
+            returnType(double(1))
+            return(propValueVector)
+        },
+        adaptiveProcedure = function(jump = logical()) {
+            timesRan <<- timesRan + 1
+            if(jump)     timesAccepted <<- timesAccepted + 1
+            if(!adaptScaleOnly)     empirSamp[timesRan, 1:d] <<- values(model, target)
+            if(timesRan %% adaptInterval == 0) {
+                acceptanceRate <- timesAccepted / timesRan
+                timesAdapted <<- timesAdapted + 1
+                if(saveMCMChistory) {
+                    setSize(scaleHistory, timesAdapted)                 ## scaleHistory
+                    scaleHistory[timesAdapted] <<- scale                ## scaleHistory
+                    setSize(acceptanceHistory, timesAdapted)            ## scaleHistory
+                    acceptanceHistory[timesAdapted] <<- acceptanceRate  ## scaleHistory
+                    if(d <= 10) {
+                        propCovTemp <- propCovHistory                                           ## scaleHistory
+                        setSize(propCovHistory, timesAdapted, d, d)                             ## scaleHistory
+                        if(timesAdapted > 1)                                                    ## scaleHistory
+                            for(iTA in 1:(timesAdapted-1))                                      ## scaleHistory
+                                propCovHistory[iTA, 1:d, 1:d] <<- propCovTemp[iTA, 1:d, 1:d]    ## scaleHistory
+                        propCovHistory[timesAdapted, 1:d, 1:d] <<- propCov[1:d, 1:d]            ## scaleHistory
+                    }
+                }
+                adaptFactor <- my_calcAdaptationFactor$run(acceptanceRate)
+                scale <<- scale * adaptFactor
+                ## calculate empirical covariance, and adapt proposal covariance
+                if(!adaptScaleOnly) {
+                    gamma1 <- my_calcAdaptationFactor$getGamma1()
+                    for(i in 1:d)     empirSamp[, i] <<- empirSamp[, i] - mean(empirSamp[, i])
+                    empirCov <- (t(empirSamp) %*% empirSamp) / (timesRan-1)
+                    propCov <<- propCov + gamma1 * (empirCov - propCov)
+                    chol_propCov <<- chol(propCov)
+                }
+                chol_propCov_scale <<- chol_propCov * scale
+                timesRan <<- 0
+                timesAccepted <<- 0
+            }
+        },
+        getScaleHistory = function() {  ## scaleHistory
+            if(!saveMCMChistory)   print("Please set 'nimbleOptions(MCMCsaveHistory = TRUE)' before building the MCMC")
+            returnType(double(1))
+            return(scaleHistory)
+        },          
+        getAcceptanceHistory = function() {  ## scaleHistory
+            returnType(double(1))
+            if(!saveMCMChistory)   print("Please set 'nimbleOptions(MCMCsaveHistory = TRUE)' before building the MCMC")
+            return(acceptanceHistory)
+        },                  
+        getPropCovHistory = function() { ## scaleHistory
+            if(!saveMCMChistory | d > 10)   print("Please set 'nimbleOptions(MCMCsaveHistory = TRUE)' before building the MCMC and note that to reduce memory use we only save the proposal covariance history for parameter vectors of length 10 or less")
+            returnType(double(3))
+            return(propCovHistory)
+        },
+        ##getScaleHistoryExpanded = function() {                                                              ## scaleHistory
+        ##    scaleHistoryExpanded <- numeric(timesAdapted*adaptInterval, init=FALSE)                         ## scaleHistory
+        ##    for(iTA in 1:timesAdapted)                                                                      ## scaleHistory
+        ##        for(j in 1:adaptInterval)                                                                   ## scaleHistory
+        ##            scaleHistoryExpanded[(iTA-1)*adaptInterval+j] <- scaleHistory[iTA]                      ## scaleHistory
+        ##    returnType(double(1)); return(scaleHistoryExpanded) },                                          ## scaleHistory
+        ##getPropCovHistoryExpanded = function() {                                                            ## scaleHistory
+        ##    propCovHistoryExpanded <- array(dim=c(timesAdapted*adaptInterval,d,d), init=FALSE)              ## scaleHistory
+        ##    for(iTA in 1:timesAdapted)                                                                      ## scaleHistory
+        ##        for(j in 1:adaptInterval)                                                                   ## scaleHistory
+        ##            propCovHistoryExpanded[(iTA-1)*adaptInterval+j,1:d,1:d] <- propCovHistory[iTA,1:d,1:d]  ## scaleHistory
+        ##    returnType(double(3)); return(propCovHistoryExpanded) },                                        ## scaleHistory
+        reset = function() {
+            scale   <<- scaleOriginal
+            propCov <<- propCovOriginal
+            chol_propCov <<- chol(propCov)
+            chol_propCov_scale <<- chol_propCov * scale
+            timesRan      <<- 0
+            timesAccepted <<- 0
+            timesAdapted  <<- 0
+            if(saveMCMChistory) {
+                scaleHistory  <<- c(0, 0)    ## scaleHistory
+                acceptanceHistory  <<- c(0, 0)
+                if(d <= 10) 
+                    propCovHistory <<- nimArray(0, dim = c(2,d,d))
+            }
+            my_calcAdaptationFactor$reset()
+        }
+    ), where = getLoadingNamespace()
+)
 
 ####################################################################################################
 ### CAR_scalar_{postPred,conjugate,RW} component samplers for dcar_{normal,proper} distributions ###
