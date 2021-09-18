f29cb7ecc8c02a45b0d80a8b8733a5a3167c3a01
Christopher Paciorek <paciorek@stat.berkeley.edu>
2021-09-18 13:20:00 -0700

    use nlines=1 in deparse to avoid multi-line output that will mess up further processing

--- packages/nimble/R/all_utils.R
+++ packages/nimble/R/all_utils.R
@@ -136,9 +136,9 @@ getsize <- function(container) {
 # simply adds width.cutoff = 500 as the default to deal with creation of long variable names from expressions
 deparse <- function(...) {
     if("width.cutoff" %in% names(list(...))) {
-        base::deparse(..., control = "digits17")
+        base::deparse(..., control = "digits17", nlines = 1)
     } else {
-        base::deparse(..., width.cutoff = 500L, control = "digits17")
+        base::deparse(..., width.cutoff = 500L, control = "digits17", nlines = 1)
     }
 }
 
