2c986c01fe388dc11413bd6ca4976588f88e3062
Christopher Paciorek <paciorek@stat.berkeley.edu>
2021-09-21 17:52:45 -0700

    fix use of warn in safeDeparse

--- packages/nimble/R/all_utils.R
+++ packages/nimble/R/all_utils.R
@@ -145,14 +145,14 @@ deparse <- function(...) {
 ## This version of deparse avoids splitting into multiple lines, which generally would lead to
 ## problems. We keep the original nimble:::deparse above as deparse is widely used and there
 ## are cases where not modifying the nlines behavior may be best. 
-safeDeparse <- function(...) {
+safeDeparse <- function(..., warn = FALSE) {
     out <- deparse(...)
     if(nimbleOptions('useSafeDeparse')) {
         dotArgs <- list(...)
         if("nlines" %in% names(dotArgs))
             nlines <- dotArgs$nlines else nlines <- 1L
         if(nlines != -1L && length(out) > nlines) {
-            if("warn" %in% names(dotArgs) && dotArgs$warn)
+            if(warn)
                 warning("safeDeparse: truncating deparse output to ", nlines, " lines.")
             out <- out[1:nlines]
         }
