9a64a921564d4dfac5452cbb474d8b6f7eb54e98
Christopher Paciorek <paciorek@stat.berkeley.edu>
2021-10-23 15:12:51 -0700

    force nimbleSMC tests to be installed

--- install_revDeps.R
+++ install_revDeps.R
@@ -4,10 +4,13 @@ revDeps <- c(
     'nimbleSMC'
     )     
 
+
+## Note that if don't specify repos, the RStudio repo seems to install from
+## Linux binary (that is what the .tar.gz is?) and doesn't install tests.
 for (package in revDeps) {
     if (!suppressPackageStartupMessages(require(package,
                                                 character.only = TRUE))) {
-        install.packages(package, repos = 'http://cran.us.r-project.org')
+        install.packages(package, type = 'source', INSTALL_opts = '--install-tests', repos = 'http://cran.us.r-project.org')
     }
 }
 
