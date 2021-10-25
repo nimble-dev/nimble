15edb8ad1e35bede8ae8277acde7c41145413a67
Christopher Paciorek <paciorek@stat.berkeley.edu>
2021-10-25 08:58:43 -0700

    trying to debug lack of installed nimbleSMC tests

--- install_revDeps.R
+++ install_revDeps.R
@@ -8,7 +8,7 @@ revDeps <- c(
 ## Note that if don't specify repos, the RStudio repo seems to install from
 ## Linux binary (that is what the .tar.gz is?) and doesn't install tests.
 for (package in revDeps) {
-        install.packages(package, type = 'source', INSTALL_opts = '--install-tests', repos = 'https://cran.us.r-project.org')
+        install.packages(package, type = 'source', INSTALL_opts = '--install-tests')
     }
     print(list.files(system.file('tests','testthat', package='nimbleSMC')))
     print(list.files(system.file('', package='nimbleSMC')))
