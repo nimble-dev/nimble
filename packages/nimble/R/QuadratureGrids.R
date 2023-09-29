9bcf156da403e8f336eebae24a7d0956ad3a150f
paul-vdb <paul.vandambates@gmail.com>
2023-09-29 16:14:39 -0700

    Overhaul of quad grid
    Added all the AGHQ and CCD to nimble functions that are accessible within the grid nimble function list quadrature. In theory this will allow a user to use any grid points they want to pass to the approx posterior object.

--- packages/nimble/R/QuadratureGrids.R
+++ packages/nimble/R/QuadratureGrids.R
@@ -1,50 +1,10 @@
-## R Function to build an AGHQ grid in multiple dimensions.
-Rget_AGHQ_nodes <- function(dm = 1, n = 3) {
-	## If n=1 or is miswritten, do Laplace.
-	if(n <= 1){	
-		z <- c(0,-1)	## For one time fixes
-		w_star <- c(sqrt(2*pi), -1)
-	}else{
-		## Gauss-Hermite Nodes
-		gh_nodes <- pracma::gaussHermite(n)
-		## Convert to z = sqrt(2)*z, and wgt = exp(x^2)*sqrt(2)
-		z <- sqrt(2) * gh_nodes$x
-		w_star <- exp(gh_nodes$x^2 + log(gh_nodes$w)) * sqrt(2)
-		if(n %% 2 != 0) z[ceiling(n/2)] <- 0	## Make it a hard zero.
-	}
-	if(dm == 1) return(list("z_nodes" = z, "weights" = w_star))
-	## Expand those nodes for n dimensions.
-	w <- rep(1, n^dm)
-	z_nodes <- matrix(0, ncol = dm, nrow = n^dm)
-	for (i in 1:dm) {
-		z_nodes[ ,i] = rep(z, each = n^(i-1), times = (n^dm/n^i))
-		w <- w * rep(w_star, each = n^(i-1), times = (n^dm/n^i))
-	}	
-	return(list("z_nodes" = as.matrix(z_nodes), "weights" = as.numeric(w)))
-}
-
-## Build a CCD grid for hyperparameters in the quadrature object.
-CCDGrid <- function(dm)
-{
-	f0 <- 1.1	## Saw 1.1 on INLA and 1.5 default for mgcv. < sqrt(2) seems to be an issue but they use a larger sphere.
-
-	z <- rsm::ccd(dm, n0 = c(1,rep(0,dm-1)), alpha = "rotatable", 
-		inscribed = TRUE, oneblock=TRUE, randomize = FALSE)
-	z <-  rsm::decode.data(z)[,-(1:2)]
-	
-	np <- nrow(z)
-	wgts <- 1 / ((np - 1) * (1 + exp(-0.5*dm*f0^2) * (f0^2 - 1)))
-	wgt0 <- 1 - (np-1)*wgts
-			
-	weights <- rep(wgts, np)
-	weights[which(rowSums(abs(z)) == 0)] <- wgt0
-	
-	return(list("z_nodes" = as.matrix(z), "weights" = as.numeric(weights)))
-}
-
+## Build a Grid Base to access all these functions in a nimble function list.
 GRID_BASE <- nimbleFunctionVirtual(
   run = function() {},
   methods = list(
+		buildAGHQ = function(){},
+		buildCCD = function(){},
+		buildGrid = function(){},
 		saveValues = function(i = integer(0),theta = double(1), logDensity = double(), 
 														innerCholesky = double(2), innerMode = double(1)){},
 		calcCheck = function(i=integer()){returnType(integer())},
@@ -61,86 +21,213 @@ GRID_BASE <- nimbleFunctionVirtual(
 	)
 )
 
-
-## Function to store values from inner optimization to be used for 
-## joint inference on the fixed and random effects.
-## Regardless of AGHQ or CCD, we need to store all this information to be
-## able to simulate for \bm{w}. Additional convenience functions added.
-
 ## Added in dimensions here so it can be updated to different grid sizes.
 ## Use an index to pass which is which. Added one time fixes if they want to
 ## do Laplace or some sort of Empirical Bayes method.
-storeGridValues <- nimbleFunction(
+# nQ - Number of quadrature points for AGHQ or custom
+# method - Grid method to use, either CCD, AGHQ, or Custom (which will require the user to input the grid in future iterations)
+# nre - Number of fixed and random effects.
+buildQuadGrid <- nimbleFunction(
 	contains = GRID_BASE,
-	setup = function(z, wgt, nre){
-		gridFix <- 0
-		if(!is.matrix(z)){
-			nTheta <- length(z)
-			nGrid <- 1
-			gridFix <- 1
-		}else{
-			nTheta <- ncol(z)
-			nGrid <- nrow(z)
-		}
-		if(nGrid == 1) {
-			z <- matrix(z, ncol = nTheta, nrow = nGrid)
-			wgt <- as.numeric(c(wgt, -1))
-		}
-		one_time_fixes_done <- FALSE
+	setup = function(d, nQ, method, nre){
+		# if(!method %in% c("CCD", "AGHQ", "Custom")) print("Only CCD, AGHQ, and Custom Grids are supported")
+		if (d > 120 | d<1) print("Dimension of Theta must be in [1,120]")		
 
-		thetaVals <- matrix(0, nrow = nGrid, ncol = nTheta)
-		reMode <- matrix(0, nrow = nGrid, ncol = nre)
-		calculated <- numeric(nGrid + gridFix)
-		cholVals <- array(0, c(nGrid + gridFix, nre, nre))
-		logDensTheta <- numeric(nGrid + gridFix)
+		## Don't let them use an even quadrature grid. Inefficient.
+		if(method == 2 & nQ %% 2 == 0) {
+			print("Even Quadrature grids are not recommended: Changing to + 1")
+			nQ <- nQ + 1	
+		}
+		## Walsh Index Assignments for Resolution V Fractional Factorials
+		index <- c(1, 2, 4, 8, 15, 16, 32, 51, 64, 85, 106, 128,
+			150, 171, 219, 237, 247, 256, 279, 297, 455, 512, 537,
+			557, 594, 643, 803, 863, 998, 1024, 1051, 1070, 1112,
+			1169, 1333, 1345, 1620, 1866, 2048, 2076, 2085, 2185,
+			2372, 2456, 2618, 2800, 2873, 3127, 3284, 3483, 3557,
+			3763, 4096, 4125, 4135, 4174, 4435, 4459, 4469, 4497,
+			4752, 5255, 5732, 5804, 5915, 6100, 6369, 6907, 7069,
+			8192, 8263, 8351, 8422, 8458, 8571, 8750, 8858, 9124,
+			9314, 9500, 10026, 10455, 10556, 11778, 11885, 11984,
+			13548, 14007, 14514, 14965, 15125, 15554, 16384, 16457,
+			16517, 16609, 16771, 16853, 17022, 17453, 17891, 18073,
+			18562, 18980, 19030, 19932, 20075, 20745, 21544, 22633,
+			23200, 24167, 25700, 26360, 26591, 26776, 28443, 28905,
+			29577, 32705)
+			
+		nCCD <- index; p <- 1
+		for (i in 1:length(index)) {
+			if (index[i]>=p) p <- p * 2
+			nCCD[i] <- p
+		}
+		nC <- nCCD[d]
+		nQ_aghq <- nQ
+
+		## Need to do a reverse of Eigen Vectors:
+		reverse <- 120:1
+
+		if( method == 1 ) nQ <- nC + 2*d + 1
+		if( method == 2 ) {
+			nQ <- nQ^d	## May be dimension reduced if we prune.
+		}
+		z <- matrix(0, nrow = nQ, ncol = d)
+
+		## Add some zeros if vectors will be too short.
+		## This is exclusively if the user requests Laplace (nQ = 1 AGHQ).
+		gridFix <- 0
+		if(nQ == 1)	{
+			gridFix <- 1
+		}
+
+		thetaVals <- matrix(0, nrow = nQ, ncol = d)
+		reMode <- matrix(0, nrow = nQ, ncol = nre)
+		cholVals <- array(0, c(nQ, nre, nre))
+		
+		## One time fixes for scalar / vector changes.
+		one_time_fixes_done <- FALSE
+		wgt <- numeric(d + gridFix)
+		calculated <- numeric(nQ + gridFix)
+		logDensTheta <- numeric(nQ + gridFix)
+		
+		## CCD mode index will be 1, but AGHQ it will be in the middle. Won't allow even nQ.
 		modeIndex <- 1
-		if(nTheta == 1 & nGrid > 1) modeIndex <- which(z == 0)
-		if(nTheta > 1 & nGrid > 1) modeIndex <- which(rowSums(abs(z)) == 0)
 	},
 	run=function(){},
 	methods = list(
     one_time_fixes = function() {
       ## Run this once after compiling; remove extraneous -1 if necessary
       if(one_time_fixes_done) return()
-      if(nGrid == 1) {
+      if(nQ == 1) {
 				calculated <<- numeric(length = 1, value = calculated[1])
 				logDensTheta <<- numeric(length = 1, value = logDensTheta[1])
 				wgt <<- numeric(length = 1, value = wgt[1])
 			}
 			one_time_fixes_done <<- TRUE
     },	
-		## Reset the sizes of the storage to change the grid if the user wants more AGHQ.
-		resetGrid = function(zNew = double(2), wgtNew = double(1)){
+		## Taken from Simon Wood's mgcv package.
+		## https://github.com/cran/mgcv/blob/master/R/inla.r
+		## However, we do scaled design following INLA such that z*zT = 1
+		## from https://github.com/hrue/r-inla/blob/devel/gmrflib/design.c
+		buildCCD = function(){
 			one_time_fixes()
-			nGrid <<- dim(zNew)[1]
-			setSize(thetaVals, c(nGrid, nTheta))
-			setSize(reMode, c(nGrid, nre))
-			setSize(calculated, nGrid)
-			setSize(cholVals, c(nGrid, nre, nre))
-			setSize(logDensTheta, nGrid)
-			setSize(z, c(nGrid, nTheta))
-			setSize(wgt, nGrid)
+			## First point is mode.
+			nQ <<- nC + 2*d + 1
+			design <- matrix(0, nQ, d)
+			for (i in 1:d) {
+				design[index[i]+2,i] <- 1
+				design[2:(nC+1),i] <- fwt(x = design[2:(nC+1),i], n = nC)
+			}
+			design <- design/sqrt(d)
 			
-			z <<- zNew
-			wgt <<- wgtNew
-			## Keep mode information, otherwise reset.
-			for( i in 1:nGrid ){
-				if( abs(sum(z[i,])) == 0 ) modeIndex <<- i
-				calculated[i] <<- 0
+			## Next are the star points on the axes. (scaled)
+			design[(nC+2):(nC + d + 1), 1:d] <- diag(d)*1
+			design[(nC + d + 2):(nC + 2*d + 1), 1:d] <- diag(d)*-1
+
+			## Weights as defined by Rue 2009. 
+			## Note that the radius is scaled so we don't need exp(-0.5*d*f0^2)
+			f0 <- 1.1 # sqrt(d) + 0.1
+			wgts <- 1 / ((nQ - 1) * (f0^2 - 1) * (1 + exp(-0.5*f0^2) ))
+			wgt0 <- 1 - (nQ-1)*wgts
+			
+			z <<- design
+			wgt <<- c(wgt0, rep(wgts, nQ-1))
+		},
+		## fast Walsh transform taken from Wood MGCV inla.
+		fwt = function(x = double(1), n = integer()) {
+			lag <- 1
+			while (lag < n) {
+			offset <-  lag * 2
+			ngroups <- length(x)/offset
+				for (group in 0:(ngroups-1)) { ## vectorized
+					j <- 1:lag + group*offset
+					k <- j + lag
+					xj <- x[j]; xk <- x[k]
+					x[j] <- xj + xk
+					x[k] <- xj - xk
+				}
+			lag <- offset
+			} ## while lag
+			returnType(double(1))
+			return(x)
+		},
+		buildAGHQ = function(){
+			one_time_fixes()
+			if( nQ_aghq == 1 ){
+				z <<- matrix(0, nrow = 1, ncol = d)
+				wgt <<- sqrt(2*pi)
+				modeIndex <<- 1
+			}else{
+				i <- 1:(nQ_aghq-1)
+				dv <- sqrt(i/2)
+				## Recreate pracma::Diag for this problem.
+				y <- matrix(0, nrow = nQ_aghq, ncol = nQ_aghq)
+				y[1:(nQ_aghq-1), 1:(nQ_aghq-1) + 1] <- diag(dv)
+				y[1:(nQ_aghq-1) + 1, 1:(nQ_aghq-1)] <- diag(dv)
+				E <- nimEigen(y, symmetric = TRUE)
+				L <- E$values	# Always biggest to smallest.
+				V <- E$vectors
+				inds <- reverse[(120-nQ_aghq+1):120]	## Hard coded to maximum d.
+				x <- L[inds]
+				## Make mode hard zero. We know nQ is odd and > 1.
+				x[ceiling(nQ_aghq / 2 ) ] <- 0
+				V <- t(V[, inds])
+				w <- sqrt(2*pi) * V[, 1]^2 * exp(x^2)
+				x <- sqrt(2) * x
+
+				## Build the multivariate quadrature rule.
+				k <- 1
+				wgt <<- rep(1, nQ)
+				for (i in 1:d) {
+					z[ ,i] <<- rep(x, each = nQ_aghq^(i-1), times = (nQ_aghq^d/nQ_aghq^i))
+					wgt <<- wgt * rep(w, each = nQ_aghq^(i-1), times = (nQ_aghq^d/nQ_aghq^i))
+				}
+				## Figure out the mode here: Probably still the middle but better check!
+				modeIndex <<- ceiling(nQ_aghq / 2 )
 			}
 		},
-		saveValues = function(i= integer(0, default = 1), theta = double(1), logDensity = double(),
+		buildGrid = function(){
+			if(method == 2) buildAGHQ()
+			if(method == 1) buildCCD()
+		},
+		## Reset the sizes of the storage to change the grid if the user wants more/less AGHQ.
+		resetGrid = function(nQUpdate = integer()){
+			one_time_fixes()
+			## If calling this, then method must be AGHQ currently. Can't 'update ccd'.
+			method <<- 2
+			nQ_aghq <<- nQUpdate
+			nQ <<- nQUpdate^d
+			
+			## Update weights and nodes.
+			buildAGHQ()
+			
+			## This will resize the storage to be saved later.
+			setSize(thetaVals, c(nQ, d))
+			setSize(reMode, c(nQ, nre))
+			setSize(calculated, nQ)
+			setSize(cholVals, c(nQ, nre, nre))
+			setSize(logDensTheta, nQ)
+			
+			## Keep mode information, otherwise reset.
+			for( i in 1:nQ ){
+				if( abs(sum(z[i,])) == 0 ) modeIndex <<- i
+				if( i != modeIndex ) calculated[i] <<- 0
+			}
+		},
+		saveValues = function(i = integer(0, default = 1), theta = double(1), logDensity = double(),
 									innerCholesky = double(2), innerMode = double(1)){
 			one_time_fixes()
-			if(i == 0) i <- modeIndex
+			if(i == 0) i <- modeIndex	## Can quickly update mode with input of zero.
 			logDensTheta[i] <<- logDensity
 			cholVals[i,,] <<- innerCholesky
 			thetaVals[i,] <<- theta
 			reMode[i,] <<- innerMode
 			calculated[i] <<- 1
 		},
+		## Skew the CCD grid according to the +/- skewed std normal on each side of mode.
+		## Matches with INLA code base. f = 1 as far as I can tell.
+		## z_local[i] = f * design->experiment[k][i]
+		##		    * (design->experiment[k][i] > 0.0 ? stdev_corr_pos[i] : stdev_corr_neg[i]);
 		skewGridPoints = function(skewSD = double(2)){
-			for( i in 1:nGrid )
+			for( i in 1:nQ )
 			{
 				for( j in 1:nTheta ){
 					sdAdj <- skewSD[j, 2]	# Positive Skew
@@ -171,7 +258,14 @@ storeGridValues <- nimbleFunction(
 # chol <- gridPts
 # cTest$resetGrid(gridPts$z, gridPts$weights)
 
+## Testing to make sure these match my existing code.
 # innerOpt_nfl <- nimbleFunctionList(GRID_BASE)
-# for( i in 1:10 ) innerOpt_nfl[[i]] <- storeGridValues(zGrid[i,], zWeights[i], nre = 5)
+# innerOpt_nfl[[1]] <- buildQuadGrid(d = 2, nQ = 3, method = 1, nre = 5)
+# innerOpt_nfl[[1]]$buildGrid()
+# plot(innerOpt_nfl[[1]]$getNodes())
+# innerOpt_nfl[[2]] <- buildQuadGrid(d = 2, nQ = 3, method = 2, nre = 5)
+# innerOpt_nfl[[2]]$buildGrid()
+# vals <- Rget_AGHQ_nodes(2, 3)
+# innerOpt_nfl[[2]]$getNodes()
 # innerOpt_nfl[[1]]$saveValues(theta = c(0,0,0), cholesky = matrix(rnorm(25), 5, 5), inner_mode = rnorm(5))
 # innerOpt_nfl[[1]]$getMode()
