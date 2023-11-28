b84d16c059318faffb606454045deb644ea16bde
Ken Kellner <ken@kenkellner.com>
2023-11-27 22:26:22 -0500

    Move code into BUGS_macros.R and add required docs

--- packages/nimble/R/BUGS_macros.R
+++ packages/nimble/R/BUGS_macros.R
@@ -177,3 +177,483 @@ model_macro_builder <- function(fun,
     ans
 }
 
+## This function recurses through a block of code and expands any submodels
+codeProcessModelMacros <- function(code,
+                                   modelInfo,
+                                   env = parent.frame(),
+                                   recursionLabels = character()) {
+    expandRecursionLabels <- function(possibleMacroName,
+                                      labels = character()) {
+        paste0(possibleMacroName,
+               if(length(labels) > 0)
+                   paste0('(expanded from ',
+                          paste(labels, collapse = '-->'),
+                          ')')
+               else
+                   character()
+               )
+    }
+    codeLength <- length(code)
+    ## First check if this is the start of a curly-bracketed block
+    if(code[[1]] == '{') {
+        if(codeLength > 1)
+            ## Recurse on each line
+            for(i in 2:codeLength){
+              macroOutput <- codeProcessModelMacros(code = code[[i]],
+                                                    modelInfo = modelInfo,
+                                                    env = env,
+                                                    recursionLabels
+                                                    )
+              code[[i]] <- macroOutput$code
+              modelInfo <- macroOutput$modelInfo
+            }
+        return(list(code=code, modelInfo=modelInfo))
+    }
+    ## If this is a for loop, recurse on the body of the loop
+    if(code[[1]] == 'for') {
+      macroOutput <- codeProcessModelMacros(code[[4]],
+                                            modelInfo = modelInfo,
+                                            env = env,
+                                            recursionLabels
+                                           )
+      code[[4]] <- macroOutput$code
+      return(list(code=code, modelInfo=macroOutput$modelInfo))
+    }
+    ## Check if this line invokes a submodel.
+    ## This can be done in two ways:
+    ## (i) node1 [<- | ~] <macro name>(...)
+    ## or
+    ## (ii) <macro name>(args)
+    ##
+    ## The first version is more BUGS-like.
+    ## The second version allows more full control.
+
+    ## Initialize possibleMacroName assuming version (ii):
+    possibleMacroName <- safeDeparse(code[[1]], warn = TRUE)
+    ## If it is really version (i), possibleMacroName will be
+    ## ~ or <- and should be updated to the call on the right-hand side:
+    if(possibleMacroName %in% c('<-', '~') && !is.name(code[[3]])) {
+        possibleMacroName <- safeDeparse(code[[3]][[1]], warn = TRUE)
+    }
+    if(exists(possibleMacroName)) { ## may need to provide an envir argument
+        possibleMacro <- get(possibleMacroName) ## ditto
+        if(inherits(possibleMacro, "model_macro")) {
+            expandedInfo <- try(possibleMacro$process(code, 
+                                                      modelInfo=modelInfo,
+                                                      .env = env
+                                                      ))
+            if(inherits(expandedInfo, 'try-error'))
+                stop(paste0("Model macro ",
+                            expandRecursionLabels(
+                                possibleMacroName,
+                                recursionLabels
+                            ),
+                            " failed."),
+                     call. = FALSE)
+            if(!is.list(expandedInfo))
+                stop(paste0("Model macro ",
+                            expandRecursionLabels(
+                                possibleMacroName,
+                                recursionLabels
+                            ),
+                            " should return a list with an element named ",
+                            "'code'.  It did not return a list."),
+                     call. = FALSE)
+            if(!is.call(expandedInfo[['code']]))
+                stop(paste0("Model macro ",
+                            expandRecursionLabels(
+                                possibleMacroName,
+                                recursionLabels
+                            ),
+                            " should return a list with an element named ",
+                            "'code' that is a call."),
+                     call. = FALSE)
+            
+            # Check for newly created parameters
+            newPars <- list(newMacroPars(code, expandedInfo$code))
+            names(newPars) <- possibleMacroName
+
+            # Add automatic comments showing code added by macros
+            if(getNimbleOption("enableMacroComments")){
+              if(getNimbleOption("codeInMacroComments")){
+                macroComment <- paste("#", deparse(code))
+              } else {
+                macroComment <- paste("#", possibleMacroName)
+              }
+              macroEnd <- "# ----"
+              if(length(recursionLabels > 0)){
+                spacer <- paste(rep("  ", length(recursionLabels)), collapse="")
+                hashes <- paste(rep("#", length(recursionLabels)+1), collapse="")
+                if(getNimbleOption("codeInMacroComments")){  
+                  macroComment <- paste0(spacer, hashes, " ", deparse(code))
+                } else {
+                  macroComment <- paste0(spacer, hashes, " ", possibleMacroName)
+                }
+                macroEnd <- paste0(spacer, hashes, " ----")
+              }
+              macroStartLine <- substitute(MACRO, list(MACRO = macroComment))
+              macroEndLine <- substitute(END, list(END = macroEnd))
+              expandedInfo$code <- as.call(c(list(quote(`{`)), list(macroStartLine, expandedInfo$code, macroEndLine)))
+            }
+
+            curPars <- modelInfo$parameters
+            expandedInfo$modelInfo$parameters <- c(curPars, newPars)
+
+            ## Return object is a list so we can ossibly extract other
+            ## content in the future.  We recurse on the returned code
+            ## to expand macros that it might contain.
+            macroOutput <- codeProcessModelMacros(expandedInfo$code,
+                                           modelInfo = expandedInfo$modelInfo,
+                                           env = env,
+                                           recursionLabels = c(recursionLabels, possibleMacroName)
+                                           )
+            return(list(code=macroOutput$code, modelInfo=macroOutput$modelInfo))
+        }
+    }
+    list(code=code, modelInfo=modelInfo)
+}
+
+# Index generator for all macros
+macroIndexCreator <- labelFunctionCreator("i")
+
+processModelMacros <- function(code, modelInfo, env){
+  # No generated parameters before any macros run, so parameters = empty list
+  macroIndexCreator(reset = TRUE)
+  modelInfo$indexCreator <- macroIndexCreator
+  modelInfo$parameters <- list()
+  macroOutput <- codeProcessModelMacros(code=code, modelInfo=modelInfo, env=env)
+  # Clean up extra brackets
+  macroOutput$code <- removeExtraBrackets(macroOutput$code)
+  # Convert factors to numeric
+  macroOutput$modelInfo$constants <- convertFactorConstantsToNumeric(macroOutput$modelInfo$constants)  
+  # Remove intermediate parameters from list and check for duplicates
+  macroOutput$modelInfo$parameters <- checkMacroPars(macroOutput$modelInfo$parameters,
+                                                     code, macroOutput$code)
+
+  list(code = macroOutput$code, modelInfo = macroOutput$modelInfo)
+}
+
+convertFactorConstantsToNumeric <- function(constants){
+  lapply(constants, function(x){
+    if(is.factor(x)){
+      x <- as.numeric(x)
+    } else if(is.character(x)){
+      if(is.null(dim(x))){
+        x <- as.numeric(as.factor(x))
+      } else {
+        d <- dim(x)
+        x <- as.numeric(as.factor(x))
+        x <- array(x, dim=d)
+      }
+    }
+    x
+  })
+}
+
+# Get parameters in a line of code generated by a macro
+getMacroParsInternal <- function(code){
+  if(is.call(code)){
+    code <- as.list(code)[2:length(code)]
+    return(lapply(code, getMacroParsInternal))
+  } else {
+    return(sapply(code, function(x) if(is.numeric(x) || x == "") return(NULL) else return(deparse(x))))
+  }
+}
+
+getMacroPars <- function(code){
+  out <- getMacroParsInternal(code)
+  out <- unlist(out)
+  out <- out[out != ""]
+  out <- out[!is.numeric(out)]
+  unique(out)
+}
+
+processCodeLine <- function(code){
+  #stopifnot(is.call(code))
+  if(is.character(code)) return(list(LHS = NULL, RHS = NULL))
+  if(is.name(code)){
+    return(list(LHS = NULL, RHS = deparse(code)))
+  }
+  if(code[[1]] == "{"){
+    return(lapply(as.list(code)[2:length(code)], processCodeLine))
+  }
+  if(code[[1]] == "for"){
+    return(lapply(as.list(code)[2:length(code)], processCodeLine))
+  } else {
+    #if(isAssignment())
+    if(as.character(code[[1]]) %in% c("~", "<-")){
+      RHS <- getMacroPars(code[[3]]) # getRHS()
+      LHS <- getMacroPars(code[[2]]) # getLHS()
+    } else {
+      RHS <- getMacroPars(code)
+      LHS <- NULL
+    }
+  }
+
+  list(LHS = LHS, RHS = RHS)
+}
+
+findAllListElementsByNameInternal <- function(inputList, name){
+  if(is.list(inputList) && name %in% names(inputList)){
+    return(inputList[[name]])
+  } else if(is.list(inputList)){
+    return(lapply(inputList, findAllListElementsByNameInternal, name = name))
+  }
+  return(NULL)
+}
+
+
+findAllListElementsByName <- function(inputList, name){
+  stopifnot(is.list(inputList))
+  out <- findAllListElementsByNameInternal(inputList, name)
+  unlist(out)
+}
+
+processCode <- function(code){
+  out <- processCodeLine(code)
+  LHS = as.character(findAllListElementsByName(out, "LHS"))
+  RHS = as.character(findAllListElementsByName(out, "RHS"))
+  list(LHS = unique(LHS), RHS = unique(RHS))
+}
+
+# Figure out which parameters a macro added by comparing with the original
+# line of code
+newMacroPars <- function(startCode, endCode){
+  startPar <- processCode(startCode)
+  endPar <- processCode(endCode)
+  LHS <- endPar$LHS[! endPar$LHS %in% unlist(startPar)]
+  if(length(LHS) == 0) LHS <- NULL
+  RHS <- endPar$RHS[! endPar$RHS %in% unlist(startPar)]
+  if(length(RHS) == 0) RHS <- NULL
+  list(LHS = LHS, RHS = RHS)
+}
+
+# Remove intermediate parameters and check for parameters generated
+# more than once
+checkMacroPars <- function(parameters, startCode, endCode){
+
+  # If no macros return empty list
+  if(length(parameters) == 0) return(parameters)
+  
+  # Find new parameters all at once
+  all_pars <- newMacroPars(startCode, endCode)
+
+  if(length(all_pars) == 0) return(NULL)
+
+  # Remove intermediate parameters that don't end up final code
+  final_pars <- lapply(parameters, function(x){
+    list(LHS = x$LHS[x$LHS %in% all_pars$LHS],
+         RHS = x$RHS[x$RHS %in% all_pars$RHS])
+  })
+  
+  # Check for duplicate parameters from previous macros and warn
+  if(length(final_pars) > 1){
+  for (i in 2:length(final_pars)){
+    this_macro <- final_pars[[i]]$LHS
+    for (j in 1:(i-1)){
+      dups <- this_macro %in% final_pars[[j]]$LHS
+      if(any(dups)){
+
+       msg <- paste0("  [Note] Macro '", names(final_pars[i]), "' declared LHS parameter(s): '",
+                paste(this_macro[dups], collapse="', '"), "'\n         previously declared by macro '",
+                names(final_pars[j]),"'")
+        messageIfVerbose(msg)
+
+        #warning(paste0("Macro '", names(final_pars[i]), "' generated parameter name(s) '",
+        #        paste(this_macro[dups], collapse="', '"), "' previously generated by macro '",
+        #        names(final_pars[j]),"'"), call.=FALSE)
+      }
+    }
+  }
+  }
+
+  # Organize multiple instances of macros into a list
+  unq_macros <- unique(names(final_pars))
+  fp <- lapply(unq_macros, function(x){
+    out <- final_pars[names(final_pars) == x]
+    names(out) <- NULL
+    out
+  })
+  names(fp) <- unq_macros
+  final_pars <- fp
+
+  final_pars
+}
+
+
+#' EXPERIMENTAL: Get list of parameter names generated by model macros
+#'
+#' Get a list of all parameter names (or certain categories of parameters)
+#' generated by model macros in the model code.
+#'
+#' @param model A NIMBLE model object
+#' @param includeLHS Include generated parameters on the left-hand side (LHS) of 
+#'  assignments (\code{<-} or \code{~}) in the output
+#' @param includeRHS Include generated parameters on the left-hand side (RHS) of 
+#'  assignments (\code{<-} or \code{~}) in the output
+#' @param includeDeterm Include deterministic generated parameters in the output
+#' @param includeStoch Include stochastic generated parameters in the output
+#' @param includeIndices Include index parameters generated for use in for loops
+#'  in the output
+#'
+#' @details Some model macros will generate new parameters to be included in the
+#'  output code. NIMBLE automatically detects these new parameters and 
+#'  records them in the model object. This function allows easy access
+#'  to this stored list, or subsets of it (see arguments).
+#'
+#' @return A named list of generated parameters, with the element names 
+#' corresponding to the original source macro.
+#'
+#' @export
+#'
+#' @examples
+#' nimbleOptions(enableModelMacros = TRUE)
+#' nimbleOptions(enableMacroComments = FALSE)
+#' nimbleOptions(verbose = FALSE)
+#'
+#' testMacro <- list(process = function(code, modelInfo, .env){
+#'   code <- quote({
+#'     for (i_ in 1:n){
+#'       mu[i_] <- alpha + beta
+#'       y[i_] ~ dnorm(0, sigma)
+#'     }
+#'     alpha ~ dnorm(0, 1)
+#'   })
+#'   list(code = code, modelInfo=modelInfo)
+#' })
+#'  class(testMacro) <- "model_macro"
+#'
+#' code <- nimbleCode({
+#'   y[1:n] ~ testMacro()
+#' })
+#'
+#' const <- list(y = rnorm(10), n = 10)
+#'
+#' mod <- nimbleModel(code, constants=const)
+#'
+#' mod$getMacroParameters()
+#' # should be list(testMacro = list(c("mu", "alpha", "beta", "sigma")))
+#'
+#' mod$getMacroParameters(includeRHS = FALSE)
+#' # should be list(testMacro = list(c("mu", "alpha")))
+getMacroParameters <- function(model, includeLHS = TRUE, includeRHS = TRUE, includeDeterm = TRUE, 
+                              includeStoch = TRUE, includeIndices = FALSE) {
+  dc <- model$modelDef$declInfo
+  out <- model$modelDef$macroParameters
+
+  #RHS/LHS
+  if(!includeRHS){
+    out <- lapply(out, function(x){
+      lapply(x, function(z){
+        z$RHS <- NULL
+        z
+      })
+    })
+  }
+
+  if(!includeLHS){
+    out <- lapply(out, function(x){
+      lapply(x, function(z){
+        z$LHS <- NULL
+        z
+      })
+    })
+  }
+
+  # Indices
+  if(!includeIndices){
+    idx <- sapply(unlist(lapply(dc, function(x) x$indexExpr)), deparse)
+    out <- lapply(out, function(x){
+      lapply(x, function(y){
+        yout <- lapply(y, function(z){
+          zout <- z[! z %in% idx]
+          if(length(zout) == 0) zout <- NULL
+          zout
+        })
+      })
+    })
+  }
+
+  # Type
+  varnames <- sapply(unlist(lapply(dc, function(x) x$targetVarExpr)), deparse)
+    type <- unlist(lapply(dc, function(x) x$type))
+    names(type) <- varnames
+    determ_pars <- names(type)[type == "determ"]
+    stoch_pars <- names(type)[type == "stoch"]
+    
+    if(!includeDeterm){
+      out <- lapply(out, function(x){
+        lapply(x, function(y){
+          yout <- lapply(y, function(z){
+            zout <- z[! z %in% determ_pars]
+            if(length(zout) == 0) zout <- NULL
+            zout
+          })
+        })
+      })
+    }
+
+  if(!includeStoch){
+    out <- lapply(out, function(x){
+      lapply(x, function(y){
+        yout <- lapply(y, function(z){
+          zout <- z[! z %in% stoch_pars]
+          if(length(zout) == 0) zout <- NULL
+          zout
+        })
+      })
+    })
+  }
+
+  # Cleanup
+  lapply(out, function(x){
+    lapply(x, function(y){
+      y <- unlist(y)
+      if(length(y) == 0) x <- NULL
+      names(y) <- NULL
+      unique(y)
+    })
+  })
+}
+
+# Add macro-generated inits to inits only if they don't already exist
+addMacroInits <- function(origInits, macroInits){
+  
+  outInits <- origInits
+
+  # If no macro-generated inits, do nothing
+  if(is.null(macroInits)) return(outInits)
+  if(length(macroInits) == 0) return(outInits)
+
+  # If original inits are null, make them
+  if(is.null(origInits)) outInits <- list()
+
+  # Get inits to add
+  # Only inits not already in original inits list
+  addInits <- macroInits[!names(macroInits) %in% names(origInits)]
+  
+  # If there are some new inits, add them
+  if(length(addInits) > 0){
+    outInits[names(addInits)] <- addInits
+  }
+  
+  outInits
+}
+
+# Remove extra brackets in BUGS code (typically resulting from macros)
+removeExtraBrackets <- function(code){
+  as.call(removeExtraBracketsInternal(code))
+}
+
+removeExtraBracketsInternal <- function(code){
+  unlist(lapply(code, function(x){
+    if(length(x) == 1) return(x)                       
+    if(x[[1]] == "{") x <- as.list(x)[2:length(x)]
+    if(is.list(x)){
+      x <- removeExtraBracketsInternal(x)
+    } else if(x[[1]] == "for"){
+      x[[4]] <- removeExtraBrackets(x[[4]])
+    }
+    x
+  }))
+}
