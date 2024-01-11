381da082ec73c3b6407a45f82bde398f4466bb83
Christopher Paciorek <paciorek@stat.berkeley.edu>
2024-01-11 10:19:20 -0800

    fix test-user test message related to missing rfoo

--- packages/nimble/tests/testthat/test-user.R
+++ packages/nimble/tests/testthat/test-user.R
@@ -558,7 +558,7 @@ test_that("Trap case where simulate function is removed", {
         })
     )
 
-    expect_error(cm <- compileNimble(m), "Missing simulation function")
+    expect_error(cm <- compileNimble(m), "is not available")
 })
  
 
