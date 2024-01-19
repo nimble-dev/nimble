e4247502972bd803f69a50ab33134ef3c2163de6
perrydv <perrydv@users.noreply.github.com>
2024-01-19 10:18:42 -0800

    Update test-errors

--- packages/nimble/tests/testthat/test-errors.R
+++ packages/nimble/tests/testthat/test-errors.R
@@ -7,8 +7,11 @@ nimbleOptions(verbose = FALSE)
 
 context("Testing of error handling.")
 
-test_that("Testing of error handling in SEXP_2_NimArr", {
+test_that("Testing of stopping on run-time size errors", {
   # First construct a nimbleFunction.
+  current_option <- getNimbleOption("stopOnSizeErrors")
+  nimbleOptions(stopOnSizeErrors = TRUE)
+  on.exit( nimbleOptions(stopOnSizeErrors = current_option ) )
   nimFun <- nimbleFunction(
     run = function (x = logical(1),
                     y = integer(1),
@@ -34,15 +37,12 @@ test_that("Testing of error handling in SEXP_2_NimArr", {
   wrong_shape1 = TRUE
   wrong_shape2 = matrix(c(1, 2, 3, 4), 2, 2)
   
-  # These DO NOT trigger errors, though they do trigger run-time error messages.
-  # Per conversation with Perry, this is not the ideal behavior, hence expect_failure().
-  # Print this message so anyone running tests knows this is ok.
-  cat("\nSeven run-time size error messages known to occur here:\n")
-  expect_failure(expect_error(compiledFun(wrong_shape1, y, z)))
-  expect_failure(expect_error(compiledFun(x, wrong_shape1, z)))
-  expect_failure(expect_error(compiledFun(x, y, wrong_shape1)))
-  expect_failure(expect_error(compiledFun(x, wrong_shape2, z)))
-  expect_failure(expect_error(compiledFun(x, y, wrong_shape2)))
+  # January 2024, branch stop-on-size-errors: We are making these nimStop by default
+  expect_error(compiledFun(wrong_shape1, y, z))
+  expect_error(compiledFun(x, wrong_shape1, z))
+  expect_error(compiledFun(x, y, wrong_shape1))
+  expect_error(compiledFun(x, wrong_shape2, z))
+  expect_error(compiledFun(x, y, wrong_shape2))
   
   # These DO trigger errors.
   expect_error(compiledFun(wrong_type, y, z))
@@ -50,6 +50,54 @@ test_that("Testing of error handling in SEXP_2_NimArr", {
   expect_error(compiledFun(x, y, wrong_type))
   expect_error(compiledFun(wrong_shape2, y, z))
 })
+
+test_that("Testing of warning on run-time size errors", {
+  # First construct a nimbleFunction.
+  current_option <- getNimbleOption("stopOnSizeErrors")
+  nimbleOptions(stopOnSizeErrors = FALSE)
+  on.exit( nimbleOptions(stopOnSizeErrors = current_option ) )
+  nimFun <- nimbleFunction(
+    run = function (x = logical(1),
+                    y = integer(1),
+                    z = double(1)) {
+      return(x + y + z)
+      returnType(double(1))
+    }
+  )
+
+  # Make sure the nimble function works on well formed inputs.
+  x <- c(FALSE, TRUE)
+  y <- c(2, 4)
+  z <- c(8.0, 16.0)
+  expectedResult <- c(10.0, 21.0)
+  expect_equal(nimFun(x, y, z), expectedResult)
+
+  # Next compile the nimbleFunction.
+  compiledFun = compileNimble(nimFun)
+  expect_equal(compiledFun(x, y, z), expectedResult)
+
+  # Finally check that compiledFun behaves as expected on invalid inputs.
+  wrong_type = c("x", "y")
+  wrong_shape1 = TRUE
+  wrong_shape2 = matrix(c(1, 2, 3, 4), 2, 2)
+
+  # January 2024, branch stop-on-size-errors: With the nimbleOption shown, these can use nimPrint instead of nimStop
+  # Hence we expect an error but we expect that to fail because execution does not stop but just emits a message.
+  # Print this message so anyone running tests knows this is ok.
+  cat("\nSeven run-time size error (really warning) messages known to occur here:\n")
+  expect_failure(expect_error(compiledFun(wrong_shape1, y, z)))
+  expect_failure(expect_error(compiledFun(x, wrong_shape1, z)))
+  expect_failure(expect_error(compiledFun(x, y, wrong_shape1)))
+  expect_failure(expect_error(compiledFun(x, wrong_shape2, z)))
+  expect_failure(expect_error(compiledFun(x, y, wrong_shape2)))
+
+  # These DO trigger errors.
+  expect_error(compiledFun(wrong_type, y, z))
+  expect_error(compiledFun(x, wrong_type, z))
+  expect_error(compiledFun(x, y, wrong_type))
+  expect_error(compiledFun(wrong_shape2, y, z))
+})
+
 
 test_that("Testing of error handling in indexing of undefined array", {
     mynf <- nimbleFunction(
