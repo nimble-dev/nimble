a199fbe376ddf048ad8c71fe4cdc9d57b8cc5fe4
Christopher Paciorek <paciorek@stat.berkeley.edu>
2024-11-12 18:11:31 -0800

    error trap dynamic index with AD producing non-scalar result (#1515)
    * Make minor edit to REL_INST.
    
    * Error trap adaptation interval < 2 for various block samplers.
    
    * Clean up messages/verbosity in `checkADsupportForDistribution`.
    
    * Trap dynamic indexing with AD and nonscalar result (issue 1500).

--- packages/nimble/R/BUGS_BUGSdecl.R
+++ packages/nimble/R/BUGS_BUGSdecl.R
@@ -743,6 +743,12 @@ getSymbolicParentNodesRecurse <- function(code, constNames = list(), indexNames 
             } 
         }
         if(indexingBracket) {
+            ## Detect dynamic indexing with non-scalar result and AD.
+            if(buildDerivs) 
+                if(detectNonscalarIndex(code) && any(sapply(code[3:length(code)], isDynamicIndex))) {
+                    code[3:length(code)] <- sapply(code[3:length(code)], stripDynamicallyIndexedWrapping)
+                    stop("found dynamic indexing in ", safeDeparse(code), " that would produce a non-scalar result. This is not supported with nimble's current automatic differentiation (AD) system. One can avoid use of AD via `buildDerivs=FALSE`. Alternatively, a work-around to allow use of AD is to construct the result using only scalar indexes, e.g., construct `ptmp[i, 1:3]` via `for(j in 1:3) ptmp[i, j] <- p[z[i], j]` to use in place of `p[z[i], 1:3]`")
+                }
             ## recurse on the index arguments
             contents <-
                 lapply(code[-c(1,2)],
