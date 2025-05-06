5d1ea500188586a6635d62c8e0564c771b84f9b2
Daniel Turek <danielturek@gmail.com>
2025-05-06 14:54:16 -0400

    removed legacy comments

--- packages/nimble/R/MCMC_configuration.R
+++ packages/nimble/R/MCMC_configuration.R
@@ -43,31 +43,14 @@ samplerConf <- setRefClass(
 
 ## 
 ## NOTES:
-## - cannot have "required = FALSE" NF methods, which return character() or character(1)
 ## - Ken is welcome to do as much testing as he's able - there will certainly be some bugs.
-## - For discussion: Chris's comment about derived quantities use of RNG
 ## - WAIC - next project for me could be porting that into a derived quantity
 ## 
 ## TODO:
 ## 
 ## CHANGES:
-## - (major) don't store up samples in mean/variance functions
-## - (major) setup arg list: (model, mcmc, interval, control).  Making this change required some care, since it's a catch-22 situation.  The setup function of buildMCMC builds the derivedQuantity functions, and hence calls their setup functions at the time of building them.  These setup functions (of teh derived quantity functions) now require the built (specialized) 'mcmc' function as an argument.  However, the specialized 'mcmc' algorithm object isn't available (it doesn't exist yet) until the call to buildMCMC finishes.  This was worked around using implementation internals of the nimbleFunction function itself and the underlying reference class object, and using on.exit() in the buildMCMC setup function.  This approach works, but should be looked at carefully.
-## - (major) recordingFrequency control argument for 'mean' and 'variance' functions now also accepts the value of 0 (which is now the default value).  When recordingFrequency=0, the value of the running statistic (mean or variance) is *only recorded once*, at the end of the final MCMC iteration of each chain.
-## - (major) added a "predictive" derived quantity function, with option "saveDeterm" (default TRUE)
-## - handling of burnin:
-## --- derived functions only operate post-burnin
-## --- the iteration number (iter), a runtime argument to derived functions run method, now reflects the *post-burnin* iteration number
-## --- the "total number of MCMC iterations" (niter) passed into the before_chain method is now actually (niter-nburnin)
-## --- the "nburnin" argument to the before_chain method is removed
 ##
 ## minor changes:
-## - new nimbleOption (MCMCreturnDerivedQuantities).  When TRUE (the default) derived quantity output will be returned by runMCMC when derived quantities are present.  When FALSE, derived quantity output will only be inluded in runMCMC output when expressly given the argument derivedQuantities = TRUE.
-## - derived quantities assumes final position in runMCMC return list
-## - if 'nodes' argument to logProb is a *named* list, then use these names for results
-## - corrected name of 'nchains' argument to be 'chain' in before_chain method
-## - new MCMC option 'MCMCassignSamplersToPosteriorPredictiveNodes' default TRUE.  When FALSE, configureMCMC doesn't assign samplers (by default) to any PP nodes
-## 
 ## 
 ## 
 ## 
