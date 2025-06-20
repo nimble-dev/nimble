d4c2e06f3adf0c0e2d777ee9e31636ef735751c7
Christopher Paciorek <paciorek@stat.berkeley.edu>
2025-06-20 15:25:16 -0700

    Fix scoping for nimbleMCMC (#1554)
    * Add missing newline in warning message.
    
    * Fix roxygen for buildMCMC.
    
    * Remove one of the refs to MCMCsuite/compareMCMCs as redundant.
    
    * Reword some text related to compareMCMCs in manual.
    
    * Fix scoping for user-def objects with nimbleMCMC (issue 1545).

--- packages/nimble/R/MCMC_run.R
+++ packages/nimble/R/MCMC_run.R
@@ -313,7 +313,8 @@ nimbleMCMC <- function(code,
                        samples = TRUE,
                        samplesAsCodaMCMC = FALSE,
                        summary = FALSE,
-                       WAIC = FALSE) {
+                       WAIC = FALSE,
+                       userEnv = parent.frame()) {
     #### process 'code' argument, to accept a filename, or a function
     ##if(is.character(code) || is.function(code)) {
     ##    if(is.function(code)) modelText <- mergeMultiLineStatements(deparse(body(code)))
@@ -327,7 +328,7 @@ nimbleMCMC <- function(code,
     if(missing(code) && missing(model)) stop('must provide either code or model argument')
     if(!samples && !summary && !WAIC) stop('no output specified, use samples = TRUE, summary = TRUE, or WAIC = TRUE')
     if(!missing(code) && inherits(code, 'modelBaseClass')) model <- code   ## let's handle it, if model object is provided as un-named first argument to nimbleMCMC
-    Rmodel <- mcmc_createModelObject(model, inits, nchains, setSeed, code, constants, data, dimensions, check)
+    Rmodel <- mcmc_createModelObject(model, inits, nchains, setSeed, code, constants, data, dimensions, check, userEnv = userEnv)
     conf <- configureMCMC(Rmodel, monitors = monitors, thin = thin, enableWAIC = WAIC, print = FALSE)
     Rmcmc <- buildMCMC(conf)
     compiledList <- compileNimble(Rmodel, Rmcmc)    ## only one compileNimble() call
