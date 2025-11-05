90406d493b94b101e627b1a2a2a1c47efc883ec1
Daniel Turek <danielturek@gmail.com>
2025-11-05 15:59:12 -0500

    draft of conjugacy calculations

--- packages/nimble/R/MCMC_conjugacy.R
+++ packages/nimble/R/MCMC_conjugacy.R
@@ -89,7 +89,7 @@ conjugacyRelationshipsInputList <- list(
              dweib       = list(param = 'lambda', contribution_shape = '1',     contribution_rate = 'coeff   * value^shape'           ),
              ddexp       = list(param = 'rate',   contribution_shape = '1',     contribution_rate = 'coeff   * abs(value-location)'   ),
              dcar_normal = list(param = 'tau',    contribution_shape = 'calc_dcar_normalConjugacyContributionShape(num, c, scale)',
-                                                  contribution_rate  = 'calc_dcar_normalConjugacyContributionRate(weights, value, scale)')),
+                                                  contribution_rate  = 'calc_dcar_normalConjugacyContributionRate(adj, weights, num, value, scale)')),
              ## dpar = list(...)    ## contribution_shape=1; contribution_rate=coeff*log(value/c) 'c is 2nd param of pareto'
          posterior = 'dgamma(shape = prior_shape + contribution_shape,
                              scale = 1 / (prior_rate + contribution_rate))'),
--- packages/nimble/R/miscFunctions.R
+++ packages/nimble/R/miscFunctions.R
@@ -143,7 +143,8 @@ calc_dwishAltParams <- nimbleFunction(
 calc_dcar_normalConjugacyContributionShape <- nimbleFunction(
     name = 'calc_dcar_normalConjugacyContributionShape',
     run = function(num = double(1), c = double(), scale = double()) {
-        XXXXXXXXXXXXXXXXXXXXX
+        N <- length(num)
+        ans <- (N-c)/2
         return(ans)
         returnType(double())
     }
@@ -154,8 +155,20 @@ calc_dcar_normalConjugacyContributionShape <- nimbleFunction(
 ## used in conjugacy definition for dgamma, to calculate 'contribution_rate' term:
 calc_dcar_normalConjugacyContributionRate <- nimbleFunction(
     name = 'calc_dcar_normalConjugacyContributionRate',
-    run = function(weights = double(1), value = double(1), scale = double()) {
-        XXXXXXXXXXXXXXXxx
+    run = function(adj = double(1), weights = double(1), num = double(1), value = double(1), scale = double()) {
+        N <- length(num)
+        L <- length(weights)
+        count <- 1L
+        ans <- 0
+        for(i in 1:N) {
+            if(num[i] > 0) {
+                for(j in 1:num[i]) {
+                    ans <- ans + weights[count] * (value[i] - value[adj[count]])^2 / 2
+                    count <- count + 1
+                }
+            }
+        }
+        if(count != L+1)   stop('gamma-CAR conjugacy calculation internal error')
         return(ans)
         returnType(double())
     }
